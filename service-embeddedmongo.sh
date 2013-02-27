83830242a43c51fe6f6b79733b96953ce7cadcf5
jussij <jussij@15ac3db4-bff1-4aba-a278-c1783e67ca60>
2013-02-27 12:27:01 +0000

    skriptipäivitetty hakemaan embedded mongo app oikeasta pakettirakenteesta

--- service-embeddedmongo.sh
+++ service-embeddedmongo.sh
@@ -5,4 +5,4 @@
 # niin prosessi ei välttämättä vapauta käytettyjä resursseja!
 # 
 cd valintalaskenta-service/
-mvn exec:java -Dexec.mainClass="fi.vm.sade.service.valintalaskenta.service.test.app.StartEmbeddedMongo" -Dexec.classpathScope="test"
+mvn exec:java -Dexec.mainClass="fi.vm.sade.valintalaskenta.app.StartEmbeddedMongo" -Dexec.classpathScope="test"
