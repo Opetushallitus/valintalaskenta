0f0cf0d8b2f359dc586317fd0b5178b44c27b2e2
jussij <jussij@15ac3db4-bff1-4aba-a278-c1783e67ca60>
2013-02-28 10:49:16 +0000

    embeddedskriptistä puuttui laskenta-service:n tarvitsema embedded profiili

--- run-embedded.sh
+++ run-embedded.sh
@@ -6,5 +6,5 @@ cd valintalaskenta-laskenta-service/
 mvn clean install -Pembedded
 cd ..
 cd valintalaskenta-ui/
-MAVEN_OPTS="-Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,address=8000,server=y,suspend=n" mvn clean install org.mortbay.jetty:maven-jetty-plugin:run -Pstartstack
+MAVEN_OPTS="-Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,address=8000,server=y,suspend=n" mvn clean install org.mortbay.jetty:maven-jetty-plugin:run -Pstartstack,embedded
 
