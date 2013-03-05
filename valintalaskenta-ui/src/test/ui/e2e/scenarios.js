40acd93f91aa31110a55cbcce37de8ccaf6e679a
jkaistinen <jkaistinen@15ac3db4-bff1-4aba-a278-c1783e67ca60>
2013-03-05 12:23:57 +0000

    VT-235: E2E testing

--- valintalaskenta-ui/src/test/ui/e2e/scenarios.js
+++ valintalaskenta-ui/src/test/ui/e2e/scenarios.js
@@ -1,26 +1,20 @@
 'use strict';
 
 describe('E2E-TESTS', function() {
-  describe('valintaperusteet', function() {
-
-
+  describe('valintalaskenta', function() {
     
     beforeEach(function() {
       browser().navigateTo('../../html/index.html');
     });
-  
 
-    describe('JavaScript addition operator', function () {  
-      it('adds two numbers together', function () {  
-          expect(1 + 2).toEqual(3);  
-      });
-    });
-    /*
+
     it('should redirect to rootpage', function() {
       browser().navigateTo('../../index.html');
-      expect(browser().location().url()).toBe('/phones');
+      pause();
+      expect(browser().location().url()).toBe('/haku/oid1/hakukohde/');
+      pause();
     });
-    */ 
+
   });  
 });
 
--- valintalaskenta-ui/src/test/ui/unit/controllersSpec.js
+++ valintalaskenta-ui/src/test/ui/unit/controllersSpec.js
@@ -3,14 +3,10 @@
 describe('LuoValintaryhmaController', function(){
   var LuoValintaryhmaController;
 
-  
-
   describe('truetest', function() {
 	  it('should ....', function() {
 	    expect(true).toBe(true);
 	  });
   });
-
-  
   
 });
