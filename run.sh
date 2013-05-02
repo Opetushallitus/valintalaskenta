60c6fc2fe01fc8fc47ff82e195dc783b9bce4125
unknown <monuh@KS-HKI-LT-002.KILOSOFT.LOCAL>
2013-05-02 15:44:42 +0300

    Build file for valintapersuteet

--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -e
-cd valintalaskenta-laskenta-service/
-mvn clean install
-cd ..
-cd valintalaskenta-ui/
-MAVEN_OPTS="-Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,address=8000,server=y,suspend=n" mvn clean install org.mortbay.jetty:maven-jetty-plugin:run -Pstartstack
-
