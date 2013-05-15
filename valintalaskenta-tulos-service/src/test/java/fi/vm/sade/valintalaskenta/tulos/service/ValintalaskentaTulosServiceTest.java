ecb93530c265260868f446fb654b5aba822cb6f5
jussijartamo <jussi@jartamo@gofore.com>
2013-05-15 10:25:59 +0300

    korjaus paketti- ja kansiorakenteeseen. ilmeisesti idean aiheuttama poikkeama

--- valintalaskenta-tulos-service/src/test/java/fi/vm/sade/valintalaskenta/tulos/service/ValintalaskentaTulosServiceTest.java
+++ valintalaskenta-tulos-service/src/test/java/fi/vm/sade/valintalaskenta/tulos/service/ValintalaskentaTulosServiceTest.java
@@ -1,11 +1,10 @@
 package fi.vm.sade.valintalaskenta.tulos.service;
 
-import com.lordofthejars.nosqlunit.annotation.UsingDataSet;
-import com.lordofthejars.nosqlunit.core.LoadStrategyEnum;
-import com.lordofthejars.nosqlunit.mongodb.MongoDbRule;
-import fi.vm.sade.valintalaskenta.domain.valintakoe.ValintakoeOsallistuminen;
-import fi.vm.sade.valintalaskenta.tulos.resource.ValintakoeResource;
-import org.junit.Before;
+import static com.lordofthejars.nosqlunit.mongodb.MongoDbRule.MongoDbRuleBuilder.newMongoDbRule;
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,10 +13,12 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import java.util.List;
+import com.lordofthejars.nosqlunit.annotation.UsingDataSet;
+import com.lordofthejars.nosqlunit.core.LoadStrategyEnum;
+import com.lordofthejars.nosqlunit.mongodb.MongoDbRule;
 
-import static com.lordofthejars.nosqlunit.mongodb.MongoDbRule.MongoDbRuleBuilder.newMongoDbRule;
-import static org.junit.Assert.assertEquals;
+import fi.vm.sade.valintalaskenta.domain.valintakoe.ValintakoeOsallistuminen;
+import fi.vm.sade.valintalaskenta.tulos.service.ValintalaskentaTulosService;
 
 @ContextConfiguration(locations = "classpath:application-context-test.xml")
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -33,16 +34,15 @@ public class ValintalaskentaTulosServiceTest {
     @Rule
     public MongoDbRule mongoDbRule = newMongoDbRule().defaultSpringMongoDb("test");
 
-
     @Test
-    @UsingDataSet(locations="initialData.json", loadStrategy=LoadStrategyEnum.CLEAN_INSERT)
+    @UsingDataSet(locations = "initialData.json", loadStrategy = LoadStrategyEnum.CLEAN_INSERT)
     public void haeValintakoeOsallistumisetTest() {
         List<ValintakoeOsallistuminen> kaikki = valintalaskentaTulosService.haeValintakoeOsallistumiset();
         assertEquals(5, kaikki.size());
     }
 
     @Test
-    @UsingDataSet(locations="initialData.json", loadStrategy=LoadStrategyEnum.CLEAN_INSERT)
+    @UsingDataSet(locations = "initialData.json", loadStrategy = LoadStrategyEnum.CLEAN_INSERT)
     public void haeValintakoeOsallistumisetByOidTest() {
         List<ValintakoeOsallistuminen> kaikki = valintalaskentaTulosService.haeValintakoeOsallistumiset("oid1");
         assertEquals(2, kaikki.size());
@@ -50,9 +50,10 @@ public class ValintalaskentaTulosServiceTest {
     }
 
     @Test
-    @UsingDataSet(locations="initialData.json", loadStrategy=LoadStrategyEnum.CLEAN_INSERT)
+    @UsingDataSet(locations = "initialData.json", loadStrategy = LoadStrategyEnum.CLEAN_INSERT)
     public void haeValintakoeOsallistumisetByHakutoiveTest() {
-        List<ValintakoeOsallistuminen> kaikki = valintalaskentaTulosService.haeValintakoeOsallistumisetByHakutoive("oid1");
+        List<ValintakoeOsallistuminen> kaikki = valintalaskentaTulosService
+                .haeValintakoeOsallistumisetByHakutoive("oid1");
         assertEquals(2, kaikki.size());
         kaikki = valintalaskentaTulosService.haeValintakoeOsallistumisetByHakutoive("oid2");
         assertEquals(1, kaikki.size());
