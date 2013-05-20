8d573fdff2b68a1279bc31cfda3274b6b8182363
kkammone <kari.kammonen@gofore.com>
2013-05-20 17:59:07 +0300

    korjattu xls tekeminen

--- valintalaskenta-tulos-service/src/main/java/fi/vm/sade/valintalaskenta/tulos/ctrl/ValintatuloslistaController.java
+++ valintalaskenta-tulos-service/src/main/java/fi/vm/sade/valintalaskenta/tulos/ctrl/ValintatuloslistaController.java
@@ -7,17 +7,20 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import fi.vm.sade.valintalaskenta.tulos.service.ExcelExportService;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
- * 
+ *
  * @author Jussi Jartamo
- * 
+ *
  *         Exporter for - valintakoetulos - valintalaskentatulos -
  *         sijoitteluntulos
  */
@@ -32,25 +35,24 @@ public class ValintatuloslistaController {
     /**
      * @return LASKENNAN TULOKSET EXCEL
      */
-    @RequestMapping(value = "valintalaskentatulos.xls", method = GET)
-    public ResponseEntity<String> valintalaskentatulos(@RequestParam("hakukohdeOid") String hakukohdeOid) {
+    @ResponseBody
+    @RequestMapping(value = "valintalaskentatulos.xls", method = GET, produces = "application/vnd.ms-excel" )
+    public String valintalaskentatulos(@RequestParam("hakukohdeOid") String hakukohdeOid) {
         assert (hakukohdeOid != null);
         LOG.debug("Excel hakukohteelle {}", hakukohdeOid);
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Content-Disposition", "attachment; filename*=UTF-8''valintalaskentatulos.xls;");
-        return new ResponseEntity<String>(excelService.exportValintalaskentatulos(hakukohdeOid), headers, HttpStatus.OK);
+        return excelService.exportValintalaskentatulos(hakukohdeOid);
+
     }
 
     /**
      * @return VALINTAKOE EXCEL
      */
-    @RequestMapping(value = "valintakoetulos.xls", method = GET)
-    public ResponseEntity<String> valintakoetulos(@RequestParam("hakukohdeOid") String hakukohdeOid) {
+    @ResponseBody
+    @RequestMapping(value = "valintakoetulos.xls", method = GET, produces = "application/vnd.ms-excel" )
+    public String valintakoetulos(@RequestParam("hakukohdeOid") String hakukohdeOid) {
         assert (hakukohdeOid != null);
         LOG.debug("Excel hakukohteelle {}", hakukohdeOid);
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Content-Disposition", "attachment; filename*=UTF-8''valintakoetulos.xls;");
-        return new ResponseEntity<String>(excelService.exportValintakoetulos(hakukohdeOid), headers, HttpStatus.OK);
+        return excelService.exportValintakoetulos(hakukohdeOid);
     }
 
 }
