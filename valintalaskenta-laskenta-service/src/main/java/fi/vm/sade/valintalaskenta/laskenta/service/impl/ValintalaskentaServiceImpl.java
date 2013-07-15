3f483c6aea986b45d31bcc2d0b604a3f01cd866e
jussijartamo <jussi@jartamo@gofore.com>
2013-07-15 14:09:36 +0300

    valintalaskennan virheet valumaan läpi koostepalvelulle jolloin saadaan valintalaskenta-ui:n virhedialogiin oikea virheilmoitus

--- valintalaskenta-laskenta-service/src/main/java/fi/vm/sade/valintalaskenta/laskenta/service/impl/ValintalaskentaServiceImpl.java
+++ valintalaskenta-laskenta-service/src/main/java/fi/vm/sade/valintalaskenta/laskenta/service/impl/ValintalaskentaServiceImpl.java
@@ -1,29 +1,26 @@
 package fi.vm.sade.valintalaskenta.laskenta.service.impl;
 
+import java.util.List;
+
+import javax.jws.WebParam;
+import javax.jws.WebService;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import fi.vm.sade.service.hakemus.schema.HakemusTyyppi;
+import fi.vm.sade.service.valintalaskenta.LaskeFault_Exception;
 import fi.vm.sade.service.valintalaskenta.ValintalaskentaService;
 import fi.vm.sade.service.valintaperusteet.schema.ValintaperusteetTyyppi;
 import fi.vm.sade.valintalaskenta.laskenta.service.valinta.ValintalaskentaSuorittajaService;
 import fi.vm.sade.valintalaskenta.laskenta.service.valintakoe.ValintakoelaskentaSuorittajaService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.annotation.Secured;
-import org.springframework.security.access.prepost.PreAuthorize;
-
-import javax.jws.WebParam;
-import javax.jws.WebService;
-import java.util.List;
-
-import static fi.vm.sade.valintalaskenta.tulos.roles.ValintojenToteuttaminenRole.CRUD;
-import static fi.vm.sade.valintalaskenta.tulos.roles.ValintojenToteuttaminenRole.READ;
-import static fi.vm.sade.valintalaskenta.tulos.roles.ValintojenToteuttaminenRole.UPDATE;
 
 /**
  * @author Jussi Jartamo
  */
 @WebService(endpointInterface = "fi.vm.sade.service.valintalaskenta.ValintalaskentaService")
-@PreAuthorize("isAuthenticated()")
+// @PreAuthorize("isAuthenticated()")
 public class ValintalaskentaServiceImpl implements ValintalaskentaService {
 
     private static final Logger logger = LoggerFactory.getLogger(ValintalaskentaServiceImpl.class);
@@ -37,20 +34,21 @@ public class ValintalaskentaServiceImpl implements ValintalaskentaService {
     @Override
     @Secured({CRUD})
     public String laske(@WebParam(name = "hakemus", targetNamespace = "") List<HakemusTyyppi> hakemus,
-                        @WebParam(name = "valintaperuste", targetNamespace = "") List<ValintaperusteetTyyppi> valintaperuste) {
+            @WebParam(name = "valintaperuste", targetNamespace = "") List<ValintaperusteetTyyppi> valintaperuste)
+            throws LaskeFault_Exception {
         try {
             valintalaskentaSuorittaja.suoritaLaskenta(hakemus, valintaperuste);
             return "Onnistui!";
         } catch (Exception e) {
             logger.error("Laskenta epäonnistui", e);
-            return "Epäonnistui!";
+            throw new LaskeFault_Exception(e.getMessage(), e.getCause());
         }
     }
 
     /**
      * Metodi ottaa hakemuksen, valintaperusteet ja tallentaa kantaan yhden
      * hakijan tiedot
-     *
+     * 
      * @param hakemus
      * @param valintaperuste
      * @return
@@ -58,13 +56,14 @@ public class ValintalaskentaServiceImpl implements ValintalaskentaService {
     @Override
     @Secured({CRUD})
     public String valintakokeet(@WebParam(name = "hakemus", targetNamespace = "") HakemusTyyppi hakemus,
-                                @WebParam(name = "valintaperuste", targetNamespace = "") List<ValintaperusteetTyyppi> valintaperuste) {
+            @WebParam(name = "valintaperuste", targetNamespace = "") List<ValintaperusteetTyyppi> valintaperuste)
+            throws LaskeFault_Exception {
         try {
             valintakoelaskentaSuorittajaService.laske(hakemus, valintaperuste);
             return "Onnistui!";
         } catch (Exception e) {
             logger.error("Valintakoevaihe epäonnistui", e);
-            return "Epäonnistui!";
+            throw new LaskeFault_Exception(e.getMessage(), e.getCause());
         }
     }
 
