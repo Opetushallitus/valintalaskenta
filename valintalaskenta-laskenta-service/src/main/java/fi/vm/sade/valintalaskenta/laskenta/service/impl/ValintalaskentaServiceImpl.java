4f8460c52d447e301fece7dc4410d80c302bdc96
jussijartamo <jussi@jartamo@gofore.com>
2013-07-19 12:39:18 +0300

    valintalaskennan security-annotaatiot muutettu

--- valintalaskenta-laskenta-service/src/main/java/fi/vm/sade/valintalaskenta/laskenta/service/impl/ValintalaskentaServiceImpl.java
+++ valintalaskenta-laskenta-service/src/main/java/fi/vm/sade/valintalaskenta/laskenta/service/impl/ValintalaskentaServiceImpl.java
@@ -1,7 +1,5 @@
 package fi.vm.sade.valintalaskenta.laskenta.service.impl;
 
-import static fi.vm.sade.valintalaskenta.tulos.roles.ValintojenToteuttaminenRole.CRUD;
-
 import java.util.List;
 
 import javax.jws.WebParam;
@@ -10,7 +8,7 @@ import javax.jws.WebService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 import fi.vm.sade.service.hakemus.schema.HakemusTyyppi;
 import fi.vm.sade.service.valintalaskenta.LaskeFault_Exception;
@@ -23,7 +21,7 @@ import fi.vm.sade.valintalaskenta.laskenta.service.valintakoe.Valintakoelaskenta
  * @author Jussi Jartamo
  */
 @WebService(endpointInterface = "fi.vm.sade.service.valintalaskenta.ValintalaskentaService")
-// @PreAuthorize("isAuthenticated()")
+@PreAuthorize("isAuthenticated()")
 public class ValintalaskentaServiceImpl implements ValintalaskentaService {
 
     private static final Logger logger = LoggerFactory.getLogger(ValintalaskentaServiceImpl.class);
@@ -35,7 +33,7 @@ public class ValintalaskentaServiceImpl implements ValintalaskentaService {
     private ValintakoelaskentaSuorittajaService valintakoelaskentaSuorittajaService;
 
     @Override
-    @Secured({ CRUD })
+    // @Secured({ CRUD })
     public String laske(@WebParam(name = "hakemus", targetNamespace = "") List<HakemusTyyppi> hakemus,
             @WebParam(name = "valintaperuste", targetNamespace = "") List<ValintaperusteetTyyppi> valintaperuste)
             throws LaskeFault_Exception {
@@ -57,7 +55,7 @@ public class ValintalaskentaServiceImpl implements ValintalaskentaService {
      * @return
      */
     @Override
-    @Secured({ CRUD })
+    // @Secured({ CRUD })
     public String valintakokeet(@WebParam(name = "hakemus", targetNamespace = "") HakemusTyyppi hakemus,
             @WebParam(name = "valintaperuste", targetNamespace = "") List<ValintaperusteetTyyppi> valintaperuste)
             throws LaskeFault_Exception {
