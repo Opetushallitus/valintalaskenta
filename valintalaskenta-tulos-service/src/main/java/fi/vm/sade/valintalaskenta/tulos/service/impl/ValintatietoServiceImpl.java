df92d595b17f1fd3568723fc4624a4d5cc37f53f
jussijartamo <jussi@jartamo@gofore.com>
2013-08-02 12:03:26 +0300

    service palautettu

--- valintalaskenta-tulos-service/src/main/java/fi/vm/sade/valintalaskenta/tulos/service/impl/ValintatietoServiceImpl.java
+++ valintalaskenta-tulos-service/src/main/java/fi/vm/sade/valintalaskenta/tulos/service/impl/ValintatietoServiceImpl.java
@@ -1,38 +1,48 @@
 package fi.vm.sade.valintalaskenta.tulos.service.impl;
 
-import java.util.*;
+import static fi.vm.sade.valintalaskenta.tulos.roles.ValintojenToteuttaminenRole.CRUD;
+import static fi.vm.sade.valintalaskenta.tulos.roles.ValintojenToteuttaminenRole.READ;
+import static fi.vm.sade.valintalaskenta.tulos.roles.ValintojenToteuttaminenRole.UPDATE;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.jws.WebParam;
 
-import fi.vm.sade.service.valintaperusteet.schema.TasasijasaantoTyyppi;
-import fi.vm.sade.service.valintatiedot.ValintatietoService;
-import fi.vm.sade.service.valintatiedot.schema.*;
-import fi.vm.sade.valintalaskenta.domain.Hakukohde;
-import fi.vm.sade.valintalaskenta.domain.Jonosija;
-import fi.vm.sade.valintalaskenta.domain.Valinnanvaihe;
-import fi.vm.sade.valintalaskenta.domain.converter.JonosijaToJonosijaDTOConverter;
-import fi.vm.sade.valintalaskenta.domain.dto.JonosijaDTO;
-import fi.vm.sade.valintalaskenta.tulos.service.ValintalaskentaTulosService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.convert.ConversionService;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.access.prepost.PreAuthorize;
 
-import static fi.vm.sade.valintalaskenta.tulos.roles.ValintojenToteuttaminenRole.CRUD;
-import static fi.vm.sade.valintalaskenta.tulos.roles.ValintojenToteuttaminenRole.READ;
-import static fi.vm.sade.valintalaskenta.tulos.roles.ValintojenToteuttaminenRole.UPDATE;
+import fi.vm.sade.service.valintaperusteet.schema.TasasijasaantoTyyppi;
+import fi.vm.sade.service.valintatiedot.ValintatietoService;
+import fi.vm.sade.service.valintatiedot.schema.HakemusOsallistuminenTyyppi;
+import fi.vm.sade.service.valintatiedot.schema.HakemusTilaTyyppi;
+import fi.vm.sade.service.valintatiedot.schema.HakijaTyyppi;
+import fi.vm.sade.service.valintatiedot.schema.HakuTyyppi;
+import fi.vm.sade.service.valintatiedot.schema.HakukohdeTyyppi;
+import fi.vm.sade.service.valintatiedot.schema.Osallistuminen;
+import fi.vm.sade.service.valintatiedot.schema.ValinnanvaiheTyyppi;
+import fi.vm.sade.service.valintatiedot.schema.ValintatapajonoTyyppi;
+import fi.vm.sade.valintalaskenta.domain.Hakukohde;
+import fi.vm.sade.valintalaskenta.domain.Jonosija;
+import fi.vm.sade.valintalaskenta.domain.Valinnanvaihe;
+import fi.vm.sade.valintalaskenta.domain.converter.JonosijaToJonosijaDTOConverter;
+import fi.vm.sade.valintalaskenta.domain.dto.JonosijaDTO;
+import fi.vm.sade.valintalaskenta.domain.valintakoe.Hakutoive;
+import fi.vm.sade.valintalaskenta.domain.valintakoe.ValinnanVaihe;
+import fi.vm.sade.valintalaskenta.domain.valintakoe.Valintakoe;
+import fi.vm.sade.valintalaskenta.domain.valintakoe.ValintakoeOsallistuminen;
+import fi.vm.sade.valintalaskenta.tulos.service.ValintalaskentaTulosService;
 
 /**
- * Created with IntelliJ IDEA.
- * User: kkammone
- * Date: 29.4.2013
- * Time: 13:24
- * To change this template use File | Settings | File Templates.
+ * Created with IntelliJ IDEA. User: kkammone Date: 29.4.2013 Time: 13:24 To
+ * change this template use File | Settings | File Templates.
  */
 @PreAuthorize("isAuthenticated()")
 public class ValintatietoServiceImpl implements ValintatietoService {
-
 
     @Autowired
     private ValintalaskentaTulosService tulosService;
@@ -41,7 +51,40 @@ public class ValintatietoServiceImpl implements ValintatietoService {
     private ConversionService conversionService;
 
     @Override
-    @Secured({READ, UPDATE, CRUD})
+    @Secured({ READ, UPDATE, CRUD })
+    public List<HakemusOsallistuminenTyyppi> haeValintatiedotHakukohteelle(
+            @WebParam(name = "hakukohdeOid", targetNamespace = "") String hakukohdeOid) {
+        List<HakemusOsallistuminenTyyppi> osallistumiset = new ArrayList<HakemusOsallistuminenTyyppi>();
+        List<ValintakoeOsallistuminen> valinnanvaiheet = tulosService
+                .haeValintakoeOsallistumisetByHakutoive(hakukohdeOid);
+        for (ValintakoeOsallistuminen koetulos : valinnanvaiheet) {
+            for (Hakutoive hakutoive : koetulos.getHakutoiveet()) {
+                for (ValinnanVaihe vaihe : hakutoive.getValinnanVaiheet()) {
+                    for (Valintakoe valintakoe : vaihe.getValintakokeet()) {
+                        HakemusOsallistuminenTyyppi h = new HakemusOsallistuminenTyyppi();
+                        h.setHakemusOid(koetulos.getHakemusOid());
+                        switch (valintakoe.getOsallistuminen()) {
+                        case OSALLISTUU:
+                            h.setOsallistuminen(Osallistuminen.OSALLISTUU);
+                            break;
+                        case EI_OSALLISTU:
+                            h.setOsallistuminen(Osallistuminen.EI_OSALLISTU);
+                            break;
+                        default:
+                            h.setOsallistuminen(Osallistuminen.MAARITTELEMATON);
+                            break;
+                        }
+                        osallistumiset.add(h);
+                    }
+                }
+            }
+        }
+
+        return osallistumiset;
+    }
+
+    @Override
+    @Secured({ READ, UPDATE, CRUD })
     public HakuTyyppi haeValintatiedot(@WebParam(name = "hakuOid", targetNamespace = "") String hakuOid) {
 
         List<Hakukohde> a = tulosService.haeLasketutValinnanvaiheetHaulle(hakuOid);
@@ -51,9 +94,9 @@ public class ValintatietoServiceImpl implements ValintatietoService {
 
         Map<String, HakukohdeTyyppi> hakukohdeMap = new HashMap<String, HakukohdeTyyppi>();
 
-        for(Hakukohde v: a) {
+        for (Hakukohde v : a) {
             HakukohdeTyyppi ht = hakukohdeMap.get(v.getOid());
-            if(ht == null) {
+            if (ht == null) {
                 ht = new HakukohdeTyyppi();
                 ht.setOid(v.getOid());
                 hakukohdeMap.put(v.getOid(), ht);
@@ -67,11 +110,11 @@ public class ValintatietoServiceImpl implements ValintatietoService {
     }
 
     private ValinnanvaiheTyyppi createValinnanvaiheTyyppi(Valinnanvaihe valinnanvaihe) {
-        ValinnanvaiheTyyppi v  =new ValinnanvaiheTyyppi();
+        ValinnanvaiheTyyppi v = new ValinnanvaiheTyyppi();
         v.setValinnanvaihe(valinnanvaihe.getJarjestysnumero());
         v.setValinnanvaiheOid(valinnanvaihe.getValinnanvaiheoid());
-        for(fi.vm.sade.valintalaskenta.domain.Valintatapajono vt : valinnanvaihe.getValintatapajono())        {
-            v.getValintatapajono().add(createValintatapajonoTyyppi(vt)) ;
+        for (fi.vm.sade.valintalaskenta.domain.Valintatapajono vt : valinnanvaihe.getValintatapajono()) {
+            v.getValintatapajono().add(createValintatapajonoTyyppi(vt));
         }
         return v;
     }
@@ -84,39 +127,36 @@ public class ValintatietoServiceImpl implements ValintatietoService {
         valintatapajonoTyyppi.setPrioriteetti(vt.getPrioriteetti());
         valintatapajonoTyyppi.setSiirretaanSijoitteluun(vt.isSiirretaanSijoitteluun());
         valintatapajonoTyyppi.setEiVarasijatayttoa(vt.getEiVarasijatayttoa());
-        if(vt.getTasasijasaanto() != null) {
+        if (vt.getTasasijasaanto() != null) {
             valintatapajonoTyyppi.setTasasijasaanto(TasasijasaantoTyyppi.valueOf(vt.getTasasijasaanto().name()));
         }
 
-        //Sorttaa jonosijat ja laita oikea jonosija tulos niille
+        // Sorttaa jonosijat ja laita oikea jonosija tulos niille
         JonosijaToJonosijaDTOConverter converter = new JonosijaToJonosijaDTOConverter();
         List<Jonosija> jonosijat = vt.getJonosijat();
         List<JonosijaDTO> jonosijaDTOs = converter.jarjestaJaLisaaJonosijaNumero(jonosijat);
 
-        //  ValintatapajonoHelper.sortJonosijat(jonosijat);
+        // ValintatapajonoHelper.sortJonosijat(jonosijat);
 
-        for(JonosijaDTO jonosija : jonosijaDTOs) {
+        for (JonosijaDTO jonosija : jonosijaDTOs) {
             HakijaTyyppi ht = new HakijaTyyppi();
             ht.setPrioriteetti(jonosija.getPrioriteetti());
 
-            if(jonosija.getTuloksenTila() == null) {
+            if (jonosija.getTuloksenTila() == null) {
                 ht.setTila(HakemusTilaTyyppi.MAARITTELEMATON);
-            }   else {
+            } else {
                 ht.setTila(HakemusTilaTyyppi.valueOf(jonosija.getTuloksenTila().name()));
             }
             ht.setHakemusOid(jonosija.getHakemusOid());
             ht.setOid(jonosija.getHakijaOid());
             ht.setJonosija(jonosija.getJonosija());
-            if(jonosija.isHarkinnanvarainen()) {
-               ht.setHarkinnanvarainen(Boolean.TRUE);
+            if (jonosija.isHarkinnanvarainen()) {
+                ht.setHarkinnanvarainen(Boolean.TRUE);
             }
-
 
             valintatapajonoTyyppi.getHakija().add(ht);
         }
         return valintatapajonoTyyppi;
     }
 
-
 }
-
