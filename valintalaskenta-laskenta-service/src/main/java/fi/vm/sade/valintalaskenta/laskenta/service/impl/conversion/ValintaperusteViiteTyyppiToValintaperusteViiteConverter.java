f7cb6ed069a171d9aca24ac9aa8d3959ee5c150a
Kalle Wuoti <kalle.wuoti@gofore.com>
2013-08-09 12:43:43 +0300

    VT-470

--- valintalaskenta-laskenta-service/src/main/java/fi/vm/sade/valintalaskenta/laskenta/service/impl/conversion/ValintaperusteViiteTyyppiToValintaperusteViiteConverter.java
+++ valintalaskenta-laskenta-service/src/main/java/fi/vm/sade/valintalaskenta/laskenta/service/impl/conversion/ValintaperusteViiteTyyppiToValintaperusteViiteConverter.java
@@ -1,15 +1,13 @@
 package fi.vm.sade.valintalaskenta.laskenta.service.impl.conversion;
 
+import fi.vm.sade.service.valintaperusteet.model.ValintaperusteViite;
+import fi.vm.sade.service.valintaperusteet.model.Valintaperustelahde;
+import fi.vm.sade.service.valintaperusteet.schema.ValintaperusteviiteTyyppi;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
-import fi.vm.sade.service.valintaperusteet.model.ValintaperusteViite;
-import fi.vm.sade.service.valintaperusteet.schema.ValintaperusteviiteTyyppi;
-
 /**
- * 
  * @author Jussi Jartamo
- * 
  */
 @Component("ValintaperusteViiteKonvertteri")
 public class ValintaperusteViiteTyyppiToValintaperusteViiteConverter implements
@@ -18,6 +16,7 @@ public class ValintaperusteViiteTyyppiToValintaperusteViiteConverter implements
     public ValintaperusteViite convert(ValintaperusteviiteTyyppi source) {
         ValintaperusteViite target = new ValintaperusteViite();
         target.setTunniste(source.getTunniste());
+        target.setLahde(Valintaperustelahde.valueOf(source.getLahde().name()));
         target.setOnPakollinen(source.isOnPakollinen());
         return target;
     }
