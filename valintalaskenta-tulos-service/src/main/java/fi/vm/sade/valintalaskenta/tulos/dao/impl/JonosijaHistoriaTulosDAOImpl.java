b0b1d9a2718213b6d001f777e3c2dace8eaf6dfa
Kalle Wuoti <kalle.wuoti@gofore.com>
2013-08-19 14:24:38 +0300

    Jonosijahistorian haun optimointi

--- valintalaskenta-tulos-service/src/main/java/fi/vm/sade/valintalaskenta/tulos/dao/impl/JonosijaHistoriaTulosDAOImpl.java
+++ valintalaskenta-tulos-service/src/main/java/fi/vm/sade/valintalaskenta/tulos/dao/impl/JonosijaHistoriaTulosDAOImpl.java
@@ -6,6 +6,7 @@ import fi.vm.sade.valintalaskenta.domain.JonosijaHistoria;
 import fi.vm.sade.valintalaskenta.domain.Valintatapajono;
 import fi.vm.sade.valintalaskenta.tulos.dao.JonosijaHistoriaTulosDAO;
 import fi.vm.sade.valintalaskenta.tulos.dao.ValintatapajonoDAO;
+import fi.vm.sade.valintalaskenta.tulos.dao.exception.DaoException;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -30,24 +31,32 @@ public class JonosijaHistoriaTulosDAOImpl implements JonosijaHistoriaTulosDAO {
 
     @Override
     public List<JonosijaHistoria> findByValintatapajonoAndVersioAndHakemusOid(String valintatapajonoOid, String hakemusOid) {
-
         DBCollection collection = datastore.getCollection(Valintatapajono.class);
         AggregationOutput aggregation = collection.aggregate(
+                // Haetaan valintatapajono oidin mukaan
                 new BasicDBObject("$match", new BasicDBObject("valintatapajonooid", valintatapajonoOid)),
-                new BasicDBObject("$sort", new BasicDBObject("versio", "-1")),
+
+                // Sortataan versiot ja valitaan niistä viimeisin
+                new BasicDBObject("$sort", new BasicDBObject("versio", -1)),
                 new BasicDBObject("$limit", 1),
+
+                // Puretaan jonosijat
                 new BasicDBObject("$project", new BasicDBObject("jonosijat", 1)),
                 new BasicDBObject("$unwind", "$jonosijat"),
+
+                // Haetaan tietyn hakemuksen jonosija ja siihen liittyvät historiat
                 new BasicDBObject("$match", new BasicDBObject("jonosijat.hakemusoid", hakemusOid)),
-                new BasicDBObject("$project", new BasicDBObject("jonosijat.historia", 1))
+                new BasicDBObject("$project", new BasicDBObject("jonosijat.historiat", 1))
         );
 
-        if (!aggregation.results().iterator().hasNext()) {
+        if (!aggregation.getCommandResult().ok()) {
+            throw new DaoException(aggregation.getCommandResult().getErrorMessage());
+        } else if (!aggregation.results().iterator().hasNext()) {
             return Collections.EMPTY_LIST;
         }
 
+        // Loopataan historiaviitteet läpi ja haetaan niitä vastaavat dokumentit
         DBObject result = aggregation.results().iterator().next();
-
         List<DBRef> historiat = (List<DBRef>) ((DBObject) result.get("jonosijat")).get("historiat");
 
         List<ObjectId> historiaIds = new ArrayList<ObjectId>();
