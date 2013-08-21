acf4c0345b96befa149a56284d2250ec1dc2c8d7
Kalle Wuoti <kalle.wuoti@gofore.com>
2013-08-21 09:03:53 +0300

    Testi JonoSijaHistoriaDAO:n aggregation-touhulle. Muutettu testit käyttämään fongon sijasta embedded mongoa (flapdoodle).

--- valintalaskenta-tulos-service/src/test/java/fi/vm/sade/valintalaskenta/tulos/dao/JonosijaHistoriaTulosDAOTest.java
+++ valintalaskenta-tulos-service/src/test/java/fi/vm/sade/valintalaskenta/tulos/dao/JonosijaHistoriaTulosDAOTest.java
@@ -3,6 +3,7 @@ package fi.vm.sade.valintalaskenta.tulos.dao;
 import com.lordofthejars.nosqlunit.annotation.UsingDataSet;
 import com.lordofthejars.nosqlunit.core.LoadStrategyEnum;
 import com.lordofthejars.nosqlunit.mongodb.MongoDbRule;
+import fi.vm.sade.valintalaskenta.domain.JonosijaHistoria;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,22 +12,24 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 import static com.lordofthejars.nosqlunit.mongodb.MongoDbRule.MongoDbRuleBuilder.newMongoDbRule;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertEquals;
 
 /**
  * User: wuoti
- * Date: 23.5.2013
- * Time: 14.57
+ * Date: 20.8.2013
+ * Time: 19.30
  */
 @ContextConfiguration(locations = "classpath:application-context-test.xml")
 @RunWith(SpringJUnit4ClassRunner.class)
 @UsingDataSet
-public class ValintapajonoDAOTest {
-
+public class JonosijaHistoriaTulosDAOTest {
     @Autowired
-    private ValintatapajonoDAO valintatapajonoDAO;
+    private JonosijaHistoriaTulosDAO jonosijaHistoriaTulosDAO;
 
     @Autowired
     private ApplicationContext applicationContext;
@@ -35,18 +38,22 @@ public class ValintapajonoDAOTest {
     public MongoDbRule mongoDbRule = newMongoDbRule().defaultSpringMongoDb("test");
 
     @Test
-    @UsingDataSet(locations = "valintatapajonoDAOInitialData.json", loadStrategy = LoadStrategyEnum.CLEAN_INSERT)
+    @UsingDataSet(locations = "jonosijaHistoriaDAOInitialData.json", loadStrategy = LoadStrategyEnum.CLEAN_INSERT)
     public void testFindByValintapajonoOidHakemusOidAndJarjestyskriteeriPrioriteetti() {
         final String valintatapajonoOid = "valintatapajonooid1";
         final String hakemusOid = "hakemusoid1";
-        final Integer jarjestyskriteeriPrioriteetti = 0;
-        final Integer jarjestyskriteeriPrioriteettiEiLoydy = 1;
+        List<JonosijaHistoria> jonosijaHistoriat =
+                jonosijaHistoriaTulosDAO.findByValintatapajonoAndVersioAndHakemusOid(valintatapajonoOid, hakemusOid);
 
-        assertNull(valintatapajonoDAO.findByValintatapajonoOidHakemusOidAndJarjestyskriteeriPrioriteetti(
-                valintatapajonoOid, hakemusOid, jarjestyskriteeriPrioriteettiEiLoydy));
+        assertEquals(2, jonosijaHistoriat.size());
+        Collections.sort(jonosijaHistoriat, new Comparator<JonosijaHistoria>() {
+            @Override
+            public int compare(JonosijaHistoria o1, JonosijaHistoria o2) {
+                return o1.getHistoria().compareTo(o2.getHistoria());
+            }
+        });
 
-        assertNotNull(valintatapajonoDAO.findByValintatapajonoOidHakemusOidAndJarjestyskriteeriPrioriteetti(
-                valintatapajonoOid, hakemusOid, jarjestyskriteeriPrioriteetti));
+        assertEquals("historia3", jonosijaHistoriat.get(0).getHistoria());
+        assertEquals("historia4", jonosijaHistoriat.get(1).getHistoria());
     }
-
 }
