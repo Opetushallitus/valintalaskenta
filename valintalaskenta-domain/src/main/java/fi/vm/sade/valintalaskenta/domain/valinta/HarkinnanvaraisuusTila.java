afbec852eaf958a16be12bc3766d84f549d6ba5c
kkammone <kari.kammonen@gofore.com>
2013-09-13 13:03:05 +0300

    harkinnanvaraisuus

--- valintalaskenta-domain/src/main/java/fi/vm/sade/valintalaskenta/domain/valinta/HarkinnanvaraisuusTila.java
+++ valintalaskenta-domain/src/main/java/fi/vm/sade/valintalaskenta/domain/valinta/HarkinnanvaraisuusTila.java
@@ -8,5 +8,5 @@ package fi.vm.sade.valintalaskenta.domain.valinta;
  * To change this template use File | Settings | File Templates.
  */
 public enum HarkinnanvaraisuusTila {
-    HYVAKSYTTY, EI_HYVAKSTYTTY
+    HYVAKSYTTY, EI_HYVAKSYTTY
 }
