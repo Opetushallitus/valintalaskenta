1a7c5b1576687e936540448067a4860269fad91e
Kalle Wuoti <kalle.wuoti@gofore.com>
2013-10-16 08:46:43 +0300

    Ylikirjoitetaan koko valintakoeosallistuminen aina laskennan yhteydessä

--- valintalaskenta-laskenta-service/src/main/java/fi/vm/sade/valintalaskenta/laskenta/dao/ValintakoeOsallistuminenDAO.java
+++ valintalaskenta-laskenta-service/src/main/java/fi/vm/sade/valintalaskenta/laskenta/dao/ValintakoeOsallistuminenDAO.java
@@ -15,4 +15,6 @@ public interface ValintakoeOsallistuminenDAO {
     void createOrUpdate(ValintakoeOsallistuminen v);
 
     List<ValintakoeOsallistuminen> readAll();
+
+    void delete(ValintakoeOsallistuminen osallistuminen);
 }
