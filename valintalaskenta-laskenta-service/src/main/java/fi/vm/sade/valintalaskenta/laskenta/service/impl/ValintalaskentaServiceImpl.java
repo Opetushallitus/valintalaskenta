9c95706e79c50cb30e187e6a46f32fd0e1690d13
jussijartamo <jussi@jartamo@gofore.com>
2014-03-19 16:49:13 +0200

    loggaus

--- valintalaskenta-laskenta-service/src/main/java/fi/vm/sade/valintalaskenta/laskenta/service/impl/ValintalaskentaServiceImpl.java
+++ valintalaskenta-laskenta-service/src/main/java/fi/vm/sade/valintalaskenta/laskenta/service/impl/ValintalaskentaServiceImpl.java
@@ -1,21 +1,24 @@
 package fi.vm.sade.valintalaskenta.laskenta.service.impl;
 
+import static fi.vm.sade.valintalaskenta.tulos.roles.ValintojenToteuttaminenRole.CRUD;
+
+import java.util.Arrays;
+import java.util.List;
+
+import javax.jws.WebParam;
+import javax.jws.WebService;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+
 import fi.vm.sade.service.hakemus.schema.HakemusTyyppi;
 import fi.vm.sade.service.valintalaskenta.LaskeFault_Exception;
 import fi.vm.sade.service.valintalaskenta.ValintalaskentaService;
 import fi.vm.sade.service.valintaperusteet.schema.ValintaperusteetTyyppi;
 import fi.vm.sade.valintalaskenta.laskenta.service.valinta.ValintalaskentaSuorittajaService;
 import fi.vm.sade.valintalaskenta.laskenta.service.valintakoe.ValintakoelaskentaSuorittajaService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
-
-import javax.jws.WebParam;
-import javax.jws.WebService;
-import java.util.List;
-
-import static fi.vm.sade.valintalaskenta.tulos.roles.ValintojenToteuttaminenRole.CRUD;
 
 /**
  * @author Jussi Jartamo
@@ -24,50 +27,57 @@ import static fi.vm.sade.valintalaskenta.tulos.roles.ValintojenToteuttaminenRole
 @PreAuthorize("isAuthenticated()")
 public class ValintalaskentaServiceImpl implements ValintalaskentaService {
 
-    private static final Logger LOG = LoggerFactory.getLogger(ValintalaskentaServiceImpl.class);
+	private static final Logger LOG = LoggerFactory
+			.getLogger(ValintalaskentaServiceImpl.class);
 
-    @Autowired
-    private ValintalaskentaSuorittajaService valintalaskentaSuorittaja;
+	@Autowired
+	private ValintalaskentaSuorittajaService valintalaskentaSuorittaja;
 
-    @Autowired
-    private ValintakoelaskentaSuorittajaService valintakoelaskentaSuorittajaService;
+	@Autowired
+	private ValintakoelaskentaSuorittajaService valintakoelaskentaSuorittajaService;
 
-    @Override
-    @PreAuthorize(CRUD)
-    public String laske(@WebParam(name = "hakemus", targetNamespace = "") List<HakemusTyyppi> hakemus,
-                        @WebParam(name = "valintaperuste", targetNamespace = "") List<ValintaperusteetTyyppi> valintaperuste)
-            throws LaskeFault_Exception {
-        try {
-            LOG.info("Suoritetaan laskenta. Hakemuksia {} kpl ja valintaperusteita {} kpl",
-                    new Object[]{hakemus.size(), valintaperuste.size()});
-            valintalaskentaSuorittaja.suoritaLaskenta(hakemus, valintaperuste);
-            return "Onnistui!";
-        } catch (Exception e) {
-            LOG.error("Laskenta epäonnistui", e);
-            throw new LaskeFault_Exception(e.getMessage(), e.getCause());
-        }
-    }
+	@Override
+	@PreAuthorize(CRUD)
+	public String laske(
+			@WebParam(name = "hakemus", targetNamespace = "") List<HakemusTyyppi> hakemus,
+			@WebParam(name = "valintaperuste", targetNamespace = "") List<ValintaperusteetTyyppi> valintaperuste)
+			throws LaskeFault_Exception {
+		try {
+			LOG.info(
+					"Suoritetaan laskenta. Hakemuksia {} kpl ja valintaperusteita {} kpl",
+					new Object[] { hakemus.size(), valintaperuste.size() });
+			valintalaskentaSuorittaja.suoritaLaskenta(hakemus, valintaperuste);
+			return "Onnistui!";
+		} catch (Exception e) {
+			LOG.error("Valintalaskennassa tapahtui virhe {} {} {}",
+					e.getMessage(), e.getCause(),
+					Arrays.toString(e.getStackTrace()));
 
-    /**
-     * Metodi ottaa hakemuksen, valintaperusteet ja tallentaa kantaan yhden
-     * hakijan tiedot
-     *
-     * @param hakemus
-     * @param valintaperuste
-     * @return
-     */
-    @Override
-    @PreAuthorize(CRUD)
-    public String valintakokeet(@WebParam(name = "hakemus", targetNamespace = "") HakemusTyyppi hakemus,
-                                @WebParam(name = "valintaperuste", targetNamespace = "") List<ValintaperusteetTyyppi> valintaperuste)
-            throws LaskeFault_Exception {
-        try {
-            valintakoelaskentaSuorittajaService.laske(hakemus, valintaperuste);
-            return "Onnistui!";
-        } catch (Exception e) {
-            LOG.error("Valintakoevaihe epäonnistui", e);
-            throw new LaskeFault_Exception(e.getMessage(), e.getCause());
-        }
-    }
+			throw new LaskeFault_Exception(e.getMessage(), e.getCause());
+		}
+	}
+
+	/**
+	 * Metodi ottaa hakemuksen, valintaperusteet ja tallentaa kantaan yhden
+	 * hakijan tiedot
+	 * 
+	 * @param hakemus
+	 * @param valintaperuste
+	 * @return
+	 */
+	@Override
+	@PreAuthorize(CRUD)
+	public String valintakokeet(
+			@WebParam(name = "hakemus", targetNamespace = "") HakemusTyyppi hakemus,
+			@WebParam(name = "valintaperuste", targetNamespace = "") List<ValintaperusteetTyyppi> valintaperuste)
+			throws LaskeFault_Exception {
+		try {
+			valintakoelaskentaSuorittajaService.laske(hakemus, valintaperuste);
+			return "Onnistui!";
+		} catch (Exception e) {
+			LOG.error("Valintakoevaihe epäonnistui", e);
+			throw new LaskeFault_Exception(e.getMessage(), e.getCause());
+		}
+	}
 
 }
