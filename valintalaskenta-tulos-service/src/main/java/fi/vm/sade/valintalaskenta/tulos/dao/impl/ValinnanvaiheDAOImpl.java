997ee6a1703f94c13e60aabf9f5ea9c0b57aa99a
jussijartamo <jussi@jartamo@gofore.com>
2014-06-08 19:22:21 +0300

    dao muutokset

--- valintalaskenta-tulos-service/src/main/java/fi/vm/sade/valintalaskenta/tulos/dao/impl/ValinnanvaiheDAOImpl.java
+++ valintalaskenta-tulos-service/src/main/java/fi/vm/sade/valintalaskenta/tulos/dao/impl/ValinnanvaiheDAOImpl.java
@@ -1,16 +1,19 @@
 package fi.vm.sade.valintalaskenta.tulos.dao.impl;
 
-import com.google.code.morphia.Datastore;
-import com.google.code.morphia.query.UpdateOperations;
-import fi.vm.sade.valintalaskenta.domain.valinta.Valinnanvaihe;
-import fi.vm.sade.valintalaskenta.domain.valinta.Valintatapajono;
-import fi.vm.sade.valintalaskenta.tulos.dao.ValinnanvaiheDAO;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import com.google.code.morphia.Datastore;
+import com.google.code.morphia.query.UpdateOperations;
+
+import fi.vm.sade.valintalaskenta.domain.valinta.Valinnanvaihe;
+import fi.vm.sade.valintalaskenta.domain.valinta.Valintatapajono;
+import fi.vm.sade.valintalaskenta.tulos.dao.ValinnanvaiheDAO;
 
 /**
  * @author Jussi Jartamo
@@ -18,71 +21,78 @@ import java.util.List;
 @Repository
 public class ValinnanvaiheDAOImpl implements ValinnanvaiheDAO {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(ValinnanvaiheDAOImpl.class);
+	private static final Logger LOGGER = LoggerFactory
+			.getLogger(ValinnanvaiheDAOImpl.class);
 
-    @Autowired
-    private Datastore datastore;
+	@Qualifier("datastore2")
+	@Autowired
+	private Datastore datastore;
 
-    @Override
-    public List<Valinnanvaihe> readByHakuOidAndHakemusOid(String hakuOid, String hakemusOid) {
-        // TODO: Tämän kyselyn voisi tehdä fiksummin alla olevalla aggregoinnilla. Muutetaan kun keritään.
-        // DBCollection collection = datastore.getCollection(Valinnanvaihe.class);
-        // collection.aggregate(
-        // new BasicDBObject("$match", new BasicDBObject("hakuOid", hakuOid).append("valintatapajonot.jonosijat.hakemusOid", hakemusOid)),
-        // new BasicDBObject("$unwind", "$valintatapajonot"),
-        // new BasicDBObject("$unwind", "$valintatapajonot.jonosijat"),
-        // new BasicDBObject("$match", new BasicDBObject("valintatapajonot.jonosijat.hakemusOid", hakemusOid)),
-        // new BasicDBObject("$group", new BasicDBObject("_id",
-        // new BasicDBObject("hakukohdeOid", "$hakukohdeOid").append("valinnanvaiheOid", "$valinnanvaiheOid"))
-        // .append("valintatapajonot", new BasicDBObject("$addToSet", "$valintatapajonot")))
-        //        );
+	@Override
+	public List<Valinnanvaihe> readByHakuOidAndHakemusOid(String hakuOid,
+			String hakemusOid) {
+		// TODO: Tämän kyselyn voisi tehdä fiksummin alla olevalla
+		// aggregoinnilla. Muutetaan kun keritään.
+		// DBCollection collection =
+		// datastore.getCollection(Valinnanvaihe.class);
+		// collection.aggregate(
+		// new BasicDBObject("$match", new BasicDBObject("hakuOid",
+		// hakuOid).append("valintatapajonot.jonosijat.hakemusOid",
+		// hakemusOid)),
+		// new BasicDBObject("$unwind", "$valintatapajonot"),
+		// new BasicDBObject("$unwind", "$valintatapajonot.jonosijat"),
+		// new BasicDBObject("$match", new
+		// BasicDBObject("valintatapajonot.jonosijat.hakemusOid", hakemusOid)),
+		// new BasicDBObject("$group", new BasicDBObject("_id",
+		// new BasicDBObject("hakukohdeOid",
+		// "$hakukohdeOid").append("valinnanvaiheOid", "$valinnanvaiheOid"))
+		// .append("valintatapajonot", new BasicDBObject("$addToSet",
+		// "$valintatapajonot")))
+		// );
 
-        return datastore.createQuery(Valinnanvaihe.class)
-                .field("hakuOid").equal(hakuOid)
-                .field("valintatapajonot.jonosijat.hakemusOid").equal(hakemusOid)
-                .asList();
+		return datastore.createQuery(Valinnanvaihe.class).field("hakuOid")
+				.equal(hakuOid).field("valintatapajonot.jonosijat.hakemusOid")
+				.equal(hakemusOid).asList();
 
+	}
 
-    }
+	@Override
+	public Valinnanvaihe findByValintatapajonoOid(String valintatapajonoOid) {
+		return datastore.createQuery(Valinnanvaihe.class)
+				.field("valintatapajonot.valintatapajonoOid")
+				.equal(valintatapajonoOid).get();
+	}
 
-    @Override
-    public Valinnanvaihe findByValintatapajonoOid(String valintatapajonoOid) {
-        return datastore.createQuery(Valinnanvaihe.class)
-                .field("valintatapajonot.valintatapajonoOid").equal(valintatapajonoOid).get();
-    }
+	@Override
+	public List<Valinnanvaihe> readByHakukohdeOid(String hakukohdeoid) {
+		return datastore.createQuery(Valinnanvaihe.class).field("hakukohdeOid")
+				.equal(hakukohdeoid).asList();
+	}
 
-    @Override
-    public List<Valinnanvaihe> readByHakukohdeOid(String hakukohdeoid) {
-        return datastore.createQuery(Valinnanvaihe.class)
-                .field("hakukohdeOid").equal(hakukohdeoid)
-                .asList();
-    }
+	@Override
+	public List<Valinnanvaihe> readByHakuOid(String hakuoid) {
+		return datastore.createQuery(Valinnanvaihe.class).field("hakuOid")
+				.equal(hakuoid).asList();
+	}
 
-    @Override
-    public List<Valinnanvaihe> readByHakuOid(String hakuoid) {
-        return datastore.createQuery(Valinnanvaihe.class)
-                .field("hakuOid").equal(hakuoid)
-                .asList();
-    }
+	@Override
+	public void create(Valinnanvaihe valinnanvaihe) {
+		datastore.save(valinnanvaihe);
+	}
 
-    @Override
-    public void create(Valinnanvaihe valinnanvaihe) {
-        datastore.save(valinnanvaihe);
-    }
+	@Override
+	public void update(Valinnanvaihe valinnanvaihe,
+			List<Valintatapajono> jonot, String hakukohdeoid, String hakuoid) {
+		UpdateOperations ops = datastore
+				.createUpdateOperations(Valinnanvaihe.class)
+				.set("valintatapajonot", jonot)
+				.set("hakukohdeOid", hakukohdeoid).set("hakuOid", hakuoid);
+		datastore.update(valinnanvaihe, ops);
+	}
 
-    @Override
-    public void update(Valinnanvaihe valinnanvaihe, List<Valintatapajono> jonot, String hakukohdeoid, String hakuoid) {
-        UpdateOperations ops = datastore.createUpdateOperations(Valinnanvaihe.class)
-                .set("valintatapajonot", jonot)
-                .set("hakukohdeOid", hakukohdeoid)
-                .set("hakuOid", hakuoid);
-        datastore.update(valinnanvaihe, ops);
-    }
-
-    @Override
-    public Valinnanvaihe haeValinnanvaihe(String valinnanvaiheOid) {
-        return datastore.find(Valinnanvaihe.class)
-                .field("valinnanvaiheOid").equal(valinnanvaiheOid)
-                .get();
-    }
+	@Override
+	public Valinnanvaihe haeValinnanvaihe(String valinnanvaiheOid) {
+		return datastore.find(Valinnanvaihe.class).field("valinnanvaiheOid")
+				.equal(valinnanvaiheOid).get();
+	}
 }
--- valintalaskenta-tulos-service/src/main/java/fi/vm/sade/valintalaskenta/tulos/dao/impl/ValintakoeOsallistuminenDAOImpl.java
+++ valintalaskenta-tulos-service/src/main/java/fi/vm/sade/valintalaskenta/tulos/dao/impl/ValintakoeOsallistuminenDAOImpl.java
@@ -1,36 +1,45 @@
 package fi.vm.sade.valintalaskenta.tulos.dao.impl;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Repository;
+
 import com.google.code.morphia.Datastore;
+
 import fi.vm.sade.valintalaskenta.domain.valintakoe.Osallistuminen;
 import fi.vm.sade.valintalaskenta.domain.valintakoe.ValintakoeOsallistuminen;
 import fi.vm.sade.valintalaskenta.tulos.dao.ValintakoeOsallistuminenDAO;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
-public class ValintakoeOsallistuminenDAOImpl implements ValintakoeOsallistuminenDAO {
+public class ValintakoeOsallistuminenDAOImpl implements
+		ValintakoeOsallistuminenDAO {
 
-    @Autowired
-    private Datastore datastore;
+	@Qualifier("datastore2")
+	@Autowired
+	private Datastore datastore;
 
-    @Override
-    public ValintakoeOsallistuminen findByHakemusOid(String hakemusOid) {
-        return datastore.find(ValintakoeOsallistuminen.class, "hakemusOid", hakemusOid).get();
-    }
+	@Override
+	public ValintakoeOsallistuminen findByHakemusOid(String hakemusOid) {
+		return datastore.find(ValintakoeOsallistuminen.class, "hakemusOid",
+				hakemusOid).get();
+	}
 
-    @Override
-    public List<ValintakoeOsallistuminen> findByHakutoive(String hakukohdeOid) {
-        return datastore.find(ValintakoeOsallistuminen.class).field("hakutoiveet.hakukohdeOid").equal(hakukohdeOid)
-                .asList();
-    }
+	@Override
+	public List<ValintakoeOsallistuminen> findByHakutoive(String hakukohdeOid) {
+		return datastore.find(ValintakoeOsallistuminen.class)
+				.field("hakutoiveet.hakukohdeOid").equal(hakukohdeOid).asList();
+	}
 
-    @Override
-    public List<ValintakoeOsallistuminen> findByHakuAndOsallistuminen(String hakuOid, Osallistuminen osallistuminen) {
-        return datastore.find(ValintakoeOsallistuminen.class)
-                .field("hakuOid").equal(hakuOid)
-                .field("hakutoiveet.valinnanVaiheet.valintakokeet.osallistuminenTulos.osallistuminen").equal(osallistuminen)
-                .asList();
-    }
+	@Override
+	public List<ValintakoeOsallistuminen> findByHakuAndOsallistuminen(
+			String hakuOid, Osallistuminen osallistuminen) {
+		return datastore
+				.find(ValintakoeOsallistuminen.class)
+				.field("hakuOid")
+				.equal(hakuOid)
+				.field("hakutoiveet.valinnanVaiheet.valintakokeet.osallistuminenTulos.osallistuminen")
+				.equal(osallistuminen).asList();
+	}
 }
