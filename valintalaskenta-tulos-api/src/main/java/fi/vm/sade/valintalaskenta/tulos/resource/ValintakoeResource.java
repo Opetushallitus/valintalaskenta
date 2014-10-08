fd8c8d255c242124f40c8278082cb3f8ba63ab3f
jussijartamo <jussi@jartamo@gofore.com>
2014-10-08 15:22:21 +0300

    rajapintalaajennos
    Conflicts:
    	valintalaskenta/valintalaskenta-tulos-api/src/main/java/fi/vm/sade/valintalaskenta/tulos/resource/ValintakoeResource.java

--- valintalaskenta-tulos-api/src/main/java/fi/vm/sade/valintalaskenta/tulos/resource/ValintakoeResource.java
+++ valintalaskenta-tulos-api/src/main/java/fi/vm/sade/valintalaskenta/tulos/resource/ValintakoeResource.java
@@ -2,7 +2,9 @@ package fi.vm.sade.valintalaskenta.tulos.resource;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -36,5 +38,14 @@ public interface ValintakoeResource {
 	@ApiOperation(value = "Hakee valintakoeosallistumiset hakukohteelle OID:n perusteella", response = ValintakoeOsallistuminenDTO.class)
 	public List<ValintakoeOsallistuminenDTO> hakuByHakutoive(
 			@ApiParam(value = "Hakukohde OID", required = true) @PathParam("hakukohdeOid") String hakukohdeOid);
+
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("hakemus/")
+	@JsonView({ JsonViews.Basic.class })
+	@ApiOperation(value = "Hakee valintakoeosallistumiset hakukohteelle OID:n perusteella", response = ValintakoeOsallistuminenDTO.class)
+	public List<ValintakoeOsallistuminenDTO> hakuByOids(
+			@ApiParam(value = "Hakukohde OIDS", required = true) List<String> hakukohdeOid);
 
 }
