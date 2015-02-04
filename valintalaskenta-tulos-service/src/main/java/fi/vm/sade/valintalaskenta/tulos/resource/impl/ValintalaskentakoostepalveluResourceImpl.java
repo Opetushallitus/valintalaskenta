ca69614d80939b2bc13885a7d4ef44b4f697d3d6
jussi.jartamo@gofore.com <jussi.jartamo@gofore.com>
2015-02-04 12:39:49 +0200

    valintatulostentuontiin json-rajapinta, backend asynkroniseksi ja käyttöliittymä käyttämään SSE-päivitystä

--- valintalaskenta-tulos-service/src/main/java/fi/vm/sade/valintalaskenta/tulos/resource/impl/ValintalaskentakoostepalveluResourceImpl.java
+++ valintalaskenta-tulos-service/src/main/java/fi/vm/sade/valintalaskenta/tulos/resource/impl/ValintalaskentakoostepalveluResourceImpl.java
@@ -15,11 +15,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Component;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.Arrays;
 import java.util.List;
 
 import static fi.vm.sade.valintalaskenta.tulos.roles.ValintojenToteuttaminenRole.READ_UPDATE_CRUD;
@@ -49,5 +48,33 @@ public class ValintalaskentakoostepalveluResourceImpl {
             @ApiParam(value = "Hakukohteen OID", required = true) @PathParam("hakukohdeoid") String hakukohdeoid) {
         return tulosService.haeValinnanvaiheetHakukohteelle(hakukohdeoid);
     }
+
+    @POST
+    @Path("{hakukohdeoid}/valinnanvaihe")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @JsonView({ JsonViews.Basic.class })
+    @ApiOperation(value = "Lisää tuloksia valinnanvaiheelle", response = ValinnanvaiheDTO.class)
+    public Response lisaaTuloksia(
+            @ApiParam(value = "Hakukohteen OID", required = true) @PathParam("hakukohdeoid") String hakukohdeoid,
+            @ApiParam(value = "Tarjoaja OID", required = true) @QueryParam("tarjoajaOid") String tarjoajaOid,
+            @ApiParam(value = "Muokattava valinnanvaihe", required = true) ValinnanvaiheDTO vaihe) {
+        try {
+            ValinnanvaiheDTO vastaus = tulosService.lisaaTuloksia(vaihe,
+                    hakukohdeoid, tarjoajaOid);
+            return Response.status(Response.Status.ACCEPTED).entity(vastaus)
+                    .build();
+        } catch (Exception e) {
+            LOGGER.error(
+                    "Valintatapajonon pisteitä ei saatu päivitettyä hakukohteelle {}, {}\r\n{}\r\n{}",
+                    hakukohdeoid, e.getMessage(),
+                    Arrays.toString(e.getStackTrace()));
+            // , new GsonBuilder()
+            // .setPrettyPrinting().create().toJson(vaihe)
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+                    .build();
+        }
+    }
+
 }
 
