f9a426c98c2d0ca0be7876be702726848f8cc189
jussi.jartamo@gofore.com <jussi.jartamo@gofore.com>
2015-03-12 08:28:54 +0200

    ylimääräiset swagger riippuvuudet siivottu pois

--- valintalaskenta-laskenta-service/src/main/java/fi/vm/sade/valintalaskenta/laskenta/resource/ValintalaskentaResourceConfig.java
+++ valintalaskenta-laskenta-service/src/main/java/fi/vm/sade/valintalaskenta/laskenta/resource/ValintalaskentaResourceConfig.java
@@ -1,7 +1,40 @@
 package fi.vm.sade.valintalaskenta.laskenta.resource;
 
+import org.glassfish.jersey.server.ResourceConfig;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import java.io.IOException;
+
 /**
  * @author Jussi Jartamo
  */
-public class ValintalaskentaResourceConfig {
+public class ValintalaskentaResourceConfig extends ResourceConfig {
+    public ValintalaskentaResourceConfig() {
+        // json output and input
+        /**
+         * CORS Filter
+         */
+        register(new ContainerResponseFilter() {
+
+            @Override
+            public void filter(ContainerRequestContext requestContext,
+                               ContainerResponseContext responseContext)
+                    throws IOException {
+                responseContext.getHeaders().add("Access-Control-Allow-Origin",
+                        "*");
+            }
+        });
+
+        packages("fi.vm.sade.valintalaskenta");
+
+        register(com.fasterxml.jackson.jaxrs.annotation.JacksonFeatures.class);
+
+        registerInstances(
+                new com.wordnik.swagger.jaxrs.listing.ResourceListingProvider(),
+                new com.wordnik.swagger.jaxrs.listing.ApiDeclarationProvider());
+        register(com.wordnik.swagger.jaxrs.listing.ApiListingResourceJSON.class);
+    }
 }
+
