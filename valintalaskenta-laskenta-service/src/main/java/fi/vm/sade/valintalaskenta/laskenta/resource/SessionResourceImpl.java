bce1a1f24e7c3181fd0dbe436f9c1b338c13f1f1
August Kilponen <august.kilponen@gmail.com>
2015-04-27 16:45:41 +0300

    BUG-304: Session vanhenemisilmoitus

---
+++ valintalaskenta-laskenta-service/src/main/java/fi/vm/sade/valintalaskenta/laskenta/resource/SessionResourceImpl.java
@@ -0,0 +1,31 @@
+package fi.vm.sade.valintalaskenta.laskenta.resource;
+
+import com.wordnik.swagger.annotations.Api;
+import com.wordnik.swagger.annotations.ApiOperation;
+import fi.vm.sade.valintalaskenta.domain.resource.SessionResource;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+
+@Controller
+@Api(value = "/session", description = "Sessionhallinta")
+public class SessionResourceImpl implements SessionResource {
+
+    @GET
+    @Path("/maxinactiveinterval")
+    @PreAuthorize("isAuthenticated()")
+    @Produces(MediaType.TEXT_PLAIN)
+    @ApiOperation(
+            value = "Palauttaa session erääntymisen aikarajan sekunteina",
+            notes = "Tarvitsee HTTP kutsun, jossa on session id",
+            response = String.class)
+    public String maxInactiveInterval(@Context HttpServletRequest req) {
+        return Integer.toString(req.getSession().getMaxInactiveInterval());
+    }
+}
