41aeff204a33f54a2b2675d1cb997bd557e97e97
Jani Rahkola <jani.rahkola@reaktor.fi>
2015-06-02 16:23:22 +0300

    equals and toString for AvainArvoDTO

--- valintalaskenta-laskenta-api/src/main/java/fi/vm/sade/valintalaskenta/domain/dto/AvainArvoDTO.java
+++ valintalaskenta-laskenta-api/src/main/java/fi/vm/sade/valintalaskenta/domain/dto/AvainArvoDTO.java
@@ -1,11 +1,22 @@
 package fi.vm.sade.valintalaskenta.domain.dto;
 
+import org.apache.commons.lang.builder.HashCodeBuilder;
+
+import java.util.Optional;
+
 /**
  * Created by jukais on 20.3.2014.
  */
 public class AvainArvoDTO {
     private String avain;
     private String arvo;
+
+    public AvainArvoDTO() {}
+
+    public AvainArvoDTO(String avain, String arvo) {
+        this.avain = avain;
+        this.arvo = arvo;
+    }
 
     public String getAvain() {
         return avain;
@@ -21,5 +32,26 @@ public class AvainArvoDTO {
 
     public void setArvo(String arvo) {
         this.arvo = arvo;
+    }
+
+    @Override
+    public String toString() {
+        return "AvainArvoDTO(" + avain + ", " + arvo + ")";
+    }
+
+    @Override
+    public int hashCode() {
+        return Optional.ofNullable(avain).orElse("").hashCode() + 27 * Optional.ofNullable(arvo).orElse("").hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj == null || !(obj instanceof AvainArvoDTO)) {
+            return false;
+        } else {
+            AvainArvoDTO a = (AvainArvoDTO)obj;
+            return Optional.ofNullable(avain).orElse("").equals(a.avain) &&
+                    Optional.ofNullable(arvo).orElse("").equals(a.arvo);
+        }
     }
 }
