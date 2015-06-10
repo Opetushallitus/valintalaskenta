21090390ece6a6935a7e4a6b2de2e8b20693ed27
Jussi Vesala <jussi.vesala@reaktor.fi>
2015-06-10 16:23:33 +0300

    tabs to spaces

--- valintalaskenta-laskenta-api/src/main/java/fi/vm/sade/valintalaskenta/domain/dto/LaskeDTO.java
+++ valintalaskenta-laskenta-api/src/main/java/fi/vm/sade/valintalaskenta/domain/dto/LaskeDTO.java
@@ -14,74 +14,74 @@ import org.apache.commons.lang.StringUtils;
  * Created by jukais on 26.3.2014.
  */
 public class LaskeDTO {
-	private final String uuid; // <- Käynnistetyn laskennan tunniste, referenssi logeihin
-	private final String hakukohdeOid;
-	private final boolean erillishaku;
-	private final List<HakemusDTO> hakemus;
-	private final List<ValintaperusteetDTO> valintaperuste;
-	private final List<ValintaperusteetHakijaryhmaDTO> hakijaryhmat;
+    private final String uuid; // <- Käynnistetyn laskennan tunniste, referenssi logeihin
+    private final String hakukohdeOid;
+    private final boolean erillishaku;
+    private final List<HakemusDTO> hakemus;
+    private final List<ValintaperusteetDTO> valintaperuste;
+    private final List<ValintaperusteetHakijaryhmaDTO> hakijaryhmat;
 
-	public LaskeDTO() {
-		this.uuid = null;
-		this.hakemus = Collections.emptyList();
-		this.valintaperuste = Collections.emptyList();
-		this.hakukohdeOid = StringUtils.EMPTY;
-		this.hakijaryhmat = Collections.emptyList();
-		this.erillishaku = false;
-	}
+    public LaskeDTO() {
+        this.uuid = null;
+        this.hakemus = Collections.emptyList();
+        this.valintaperuste = Collections.emptyList();
+        this.hakukohdeOid = StringUtils.EMPTY;
+        this.hakijaryhmat = Collections.emptyList();
+        this.erillishaku = false;
+    }
 
-	public LaskeDTO(
-			String uuid,
-			boolean erillishaku, String hakukohdeOid,
-			List<HakemusDTO> hakemus, List<ValintaperusteetDTO> valintaperuste) {
-		this.uuid = uuid;
-		this.hakukohdeOid = hakukohdeOid;
-		this.hakemus = hakemus != null ? hakemus : Collections
-				.<HakemusDTO> emptyList();
-		this.valintaperuste = valintaperuste != null ? valintaperuste
-				: Collections.<ValintaperusteetDTO> emptyList();
-		this.hakijaryhmat = Collections.emptyList();
-		this.erillishaku = erillishaku;
-	}
+    public LaskeDTO(
+            String uuid,
+            boolean erillishaku, String hakukohdeOid,
+            List<HakemusDTO> hakemus, List<ValintaperusteetDTO> valintaperuste) {
+        this.uuid = uuid;
+        this.hakukohdeOid = hakukohdeOid;
+        this.hakemus = hakemus != null ? hakemus : Collections
+                .<HakemusDTO>emptyList();
+        this.valintaperuste = valintaperuste != null ? valintaperuste
+                : Collections.<ValintaperusteetDTO>emptyList();
+        this.hakijaryhmat = Collections.emptyList();
+        this.erillishaku = erillishaku;
+    }
 
-	public LaskeDTO(
-			String uuid,
-			boolean erillishaku, String hakukohdeOid,
-			List<HakemusDTO> hakemus, List<ValintaperusteetDTO> valintaperuste,
-			List<ValintaperusteetHakijaryhmaDTO> hakijaryhmat) {
-		this.uuid = uuid;
-		this.hakukohdeOid = hakukohdeOid;
-		this.hakemus = hakemus != null ? hakemus : Collections
-				.<HakemusDTO> emptyList();
-		this.valintaperuste = valintaperuste != null ? valintaperuste
-				: Collections.<ValintaperusteetDTO> emptyList();
-		this.hakijaryhmat = hakijaryhmat != null ? hakijaryhmat : Collections
-				.<ValintaperusteetHakijaryhmaDTO> emptyList();
-		this.erillishaku = erillishaku;
-	}
+    public LaskeDTO(
+            String uuid,
+            boolean erillishaku, String hakukohdeOid,
+            List<HakemusDTO> hakemus, List<ValintaperusteetDTO> valintaperuste,
+            List<ValintaperusteetHakijaryhmaDTO> hakijaryhmat) {
+        this.uuid = uuid;
+        this.hakukohdeOid = hakukohdeOid;
+        this.hakemus = hakemus != null ? hakemus : Collections
+                .<HakemusDTO>emptyList();
+        this.valintaperuste = valintaperuste != null ? valintaperuste
+                : Collections.<ValintaperusteetDTO>emptyList();
+        this.hakijaryhmat = hakijaryhmat != null ? hakijaryhmat : Collections
+                .<ValintaperusteetHakijaryhmaDTO>emptyList();
+        this.erillishaku = erillishaku;
+    }
 
-	public String getUuid() {
-		return uuid;
-	}
+    public String getUuid() {
+        return uuid;
+    }
 
-	public String getHakukohdeOid() {
-		return hakukohdeOid;
-	}
+    public String getHakukohdeOid() {
+        return hakukohdeOid;
+    }
 
-	public List<HakemusDTO> getHakemus() {
-		return hakemus;
-	}
+    public List<HakemusDTO> getHakemus() {
+        return hakemus;
+    }
 
-	public List<ValintaperusteetDTO> getValintaperuste() {
-		return valintaperuste;
-	}
+    public List<ValintaperusteetDTO> getValintaperuste() {
+        return valintaperuste;
+    }
 
-	public List<ValintaperusteetHakijaryhmaDTO> getHakijaryhmat() {
-		return hakijaryhmat;
-	}
+    public List<ValintaperusteetHakijaryhmaDTO> getHakijaryhmat() {
+        return hakijaryhmat;
+    }
 
-	public boolean isErillishaku() {
-		return erillishaku;
-	}
+    public boolean isErillishaku() {
+        return erillishaku;
+    }
 
 }
