8e62f654da2c1eac2c55abda9c4552d0874fea15
Jussi Vesala <jussi.vesala@reaktor.fi>
2015-06-10 16:32:09 +0300

    remove comments and stylize

--- valintalaskenta-tulos-api/src/main/java/fi/vm/sade/valintalaskenta/tulos/resource/HakuResource.java
+++ valintalaskenta-tulos-api/src/main/java/fi/vm/sade/valintalaskenta/tulos/resource/HakuResource.java
@@ -14,22 +14,18 @@ import com.wordnik.swagger.annotations.ApiOperation;
 import fi.vm.sade.valintalaskenta.domain.dto.HakukohdeDTO;
 import fi.vm.sade.valintalaskenta.domain.dto.valintakoe.ValintakoeOsallistuminenDTO;
 
-/**
- * @author Jussi Jartamo
- */
 @Path("haku")
 @Api(value = "/haku", description = "Resurssi haun valintalaskennan virhetilanteiden hakemiseen")
 public interface HakuResource {
 
-	@GET
-	@Path("{hakuOid}/virheet")
-	@Produces(MediaType.APPLICATION_JSON)
-	@ApiOperation(value = "Hakee haun valintalaskennan virhetilanteet OID:n perusteella", response = HakukohdeDTO.class)
-	public List<HakukohdeDTO> virheet(@PathParam("hakuOid") String hakuOid);
+    @GET
+    @Path("{hakuOid}/virheet")
+    @Produces(MediaType.APPLICATION_JSON)
+    @ApiOperation(value = "Hakee haun valintalaskennan virhetilanteet OID:n perusteella", response = HakukohdeDTO.class)
+    public List<HakukohdeDTO> virheet(@PathParam("hakuOid") String hakuOid);
 
-	@GET
-	@Path("{hakuOid}/valintakoevirheet")
-	@Produces(MediaType.APPLICATION_JSON)
-	public List<ValintakoeOsallistuminenDTO> valintakoevirheet(
-			@PathParam("hakuOid") String hakuOid);
+    @GET
+    @Path("{hakuOid}/valintakoevirheet")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<ValintakoeOsallistuminenDTO> valintakoevirheet(@PathParam("hakuOid") String hakuOid);
 }
