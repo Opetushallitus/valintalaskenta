b93e3c6a131240eb05c8358a91748f5851bea665
Jussi Vesala <jussi.vesala@reaktor.fi>
2015-06-10 16:38:50 +0300

    remove comments and stylize

--- valintalaskenta-tulos-service/src/main/java/fi/vm/sade/valintalaskenta/tulos/dao/impl/MuokattuJonosijaDAOImpl.java
+++ valintalaskenta-tulos-service/src/main/java/fi/vm/sade/valintalaskenta/tulos/dao/impl/MuokattuJonosijaDAOImpl.java
@@ -15,61 +15,45 @@ import fi.vm.sade.valintalaskenta.tulos.dao.MuokattuJonosijaDAO;
 
 import javax.annotation.PostConstruct;
 
-/**
- * Created with IntelliJ IDEA. User: kkammone Date: 5.8.2013 Time: 15:04 To
- * change this template use File | Settings | File Templates.
- */
 @Component
 public class MuokattuJonosijaDAOImpl implements MuokattuJonosijaDAO {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MuokattuJonosijaDAOImpl.class);
 
-	private static final Logger LOGGER = LoggerFactory
-			.getLogger(MuokattuJonosijaDAOImpl.class);
-
-	@Qualifier("datastore2")
-	@Autowired
-	private Datastore datastore;
+    @Qualifier("datastore2")
+    @Autowired
+    private Datastore datastore;
 
     @PostConstruct
     public void ensureIndexes() {
         datastore.ensureIndexes(MuokattuJonosija.class);
     }
 
-	/*
-	 * @Override public List<MuokattuJonosija> readByValintatapajonoOid(String
-	 * valintatapajonoOid) { return datastore.find(MuokattuJonosija.class)
-	 * .filter("valintatapajonoOid", valintatapajonoOid) .asList(); }
-	 */
+    @Override
+    public List<MuokattuJonosija> readByHakuOid(String hakuOid) {
+        return datastore.find(MuokattuJonosija.class).filter("hakuOid", hakuOid).asList();
+    }
 
-	@Override
-	public List<MuokattuJonosija> readByHakuOid(String hakuOid) {
-		return datastore.find(MuokattuJonosija.class)
-				.filter("hakuOid", hakuOid).asList();
-	}
+    @Override
+    public List<MuokattuJonosija> readByhakukohdeOid(String hakukohdeOid) {
+        return datastore.find(MuokattuJonosija.class).filter("hakukohdeOid", hakukohdeOid).asList();
+    }
 
-	@Override
-	public List<MuokattuJonosija> readByhakukohdeOid(String hakukohdeOid) {
-		return datastore.find(MuokattuJonosija.class)
-				.filter("hakukohdeOid", hakukohdeOid).asList();
-	}
+    @Override
+    public List<MuokattuJonosija> readByHakuOidAndHakemusOid(String hakuOid, String hakemusOid) {
+        return datastore.find(MuokattuJonosija.class)
+                .filter("hakuOid", hakuOid).filter("hakemusOid", hakemusOid)
+                .asList();
+    }
 
-	@Override
-	public List<MuokattuJonosija> readByHakuOidAndHakemusOid(String hakuOid,
-			String hakemusOid) {
-		return datastore.find(MuokattuJonosija.class)
-				.filter("hakuOid", hakuOid).filter("hakemusOid", hakemusOid)
-				.asList();
-	}
+    @Override
+    public MuokattuJonosija readByValintatapajonoOid(String valintatapajonoOid, String hakemusOid) {
+        return datastore.find(MuokattuJonosija.class)
+                .filter("valintatapajonoOid", valintatapajonoOid)
+                .filter("hakemusOid", hakemusOid).get();
+    }
 
-	@Override
-	public MuokattuJonosija readByValintatapajonoOid(String valintatapajonoOid,
-			String hakemusOid) {
-		return datastore.find(MuokattuJonosija.class)
-				.filter("valintatapajonoOid", valintatapajonoOid)
-				.filter("hakemusOid", hakemusOid).get();
-	}
-
-	@Override
-	public void saveOrUpdate(MuokattuJonosija muokattuJonosija) {
-		datastore.save(muokattuJonosija);
-	}
+    @Override
+    public void saveOrUpdate(MuokattuJonosija muokattuJonosija) {
+        datastore.save(muokattuJonosija);
+    }
 }
