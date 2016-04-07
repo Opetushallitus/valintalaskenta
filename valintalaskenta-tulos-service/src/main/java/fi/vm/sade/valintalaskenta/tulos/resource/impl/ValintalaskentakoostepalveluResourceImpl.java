30f3a035e80a218dc900b8b447fa77c81d39ad5c
jussi.jartamo@gofore.com <jussi.jartamo@gofore.com>
2016-04-07 14:02:27 +0300

    valintakoelaskennan osallistumiset palomuurin takaa

--- valintalaskenta-tulos-service/src/main/java/fi/vm/sade/valintalaskenta/tulos/resource/impl/ValintalaskentakoostepalveluResourceImpl.java
+++ valintalaskenta-tulos-service/src/main/java/fi/vm/sade/valintalaskenta/tulos/resource/impl/ValintalaskentakoostepalveluResourceImpl.java
@@ -4,11 +4,16 @@ import com.wordnik.swagger.annotations.Api;
 import com.wordnik.swagger.annotations.ApiOperation;
 import com.wordnik.swagger.annotations.ApiParam;
 import fi.vm.sade.valintalaskenta.domain.dto.ValinnanvaiheDTO;
+import fi.vm.sade.valintalaskenta.domain.dto.valintakoe.ValintakoeOsallistuminenDTO;
+import fi.vm.sade.valintalaskenta.domain.dto.valintatieto.HakemusOsallistuminenDTO;
 import fi.vm.sade.valintalaskenta.domain.dto.valintatieto.ValintatietoValinnanvaiheDTO;
+import fi.vm.sade.valintalaskenta.tulos.mapping.ValintalaskentaModelMapper;
 import fi.vm.sade.valintalaskenta.tulos.service.ValintalaskentaTulosService;
+import fi.vm.sade.valintalaskenta.tulos.service.impl.ValintatietoService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 
@@ -17,17 +22,52 @@ import javax.ws.rs.core.MediaType;
 import java.util.Arrays;
 import java.util.List;
 
+import static fi.vm.sade.valintalaskenta.tulos.roles.ValintojenToteuttaminenRole.READ_UPDATE_CRUD;
+
 @Controller
-@Path("valintalaskentakoostepalvelu/hakukohde")
-@Api(value = "/valintalaskentakoostepalvelu/hakukohde", description = "Resurssi tulosten hakemiseen hakukohteittain")
+@Path("valintalaskentakoostepalvelu")
+@Api(value = "/valintalaskentakoostepalvelu", description = "Resurssi tulosten hakemiseen hakukohteittain")
 public class ValintalaskentakoostepalveluResourceImpl {
     protected static final Logger LOGGER = LoggerFactory.getLogger(ValintalaskentakoostepalveluResourceImpl.class);
 
     @Autowired
     private ValintalaskentaTulosService tulosService;
 
+    @Autowired
+    private ValintalaskentaModelMapper modelMapper;
+    @Autowired
+    private ValintatietoService valintatietoService;
+
+    @POST
+    @Path("valintatieto/hakukohde/{hakukohdeOid}")
+    @Consumes("application/json")
+    @Produces("application/json")
+    public List<HakemusOsallistuminenDTO> haeValintatiedotHakukohteelle(@PathParam("hakukohdeOid") String hakukohdeOid,
+                                                                        List<String> valintakoeTunnisteet) {
+        return valintatietoService.haeValintatiedotHakukohteelle(valintakoeTunnisteet, hakukohdeOid);
+    }
+
     @GET
-    @Path("{hakukohdeoid}/valinnanvaihe")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("valintakoe/hakutoive/{hakukohdeOid}")
+    @ApiOperation(value = "Hakee valintakoeosallistumiset hakukohteelle OID:n perusteella", response = ValintakoeOsallistuminenDTO.class)
+    public List<ValintakoeOsallistuminenDTO> hakuByHakutoive(
+            @ApiParam(value = "Hakukohde OID", required = true) @PathParam("hakukohdeOid") String hakukohdeOid) {
+        return modelMapper.mapList(tulosService.haeValintakoeOsallistumisetByHakutoive(hakukohdeOid), ValintakoeOsallistuminenDTO.class);
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("valintakoe/hakutoive/")
+    @ApiOperation(value = "Hakee valintakoeosallistumiset hakukohteelle OID:n perusteella", response = ValintakoeOsallistuminenDTO.class)
+    public List<ValintakoeOsallistuminenDTO> hakuByOids(
+            List<String> hakukohdeOids) {
+        return modelMapper.mapList(tulosService.haeValintakoeOsallistumiset(hakukohdeOids), ValintakoeOsallistuminenDTO.class);
+    }
+
+    @GET
+    @Path("/hakukohde/{hakukohdeoid}/valinnanvaihe")
     @Produces(MediaType.APPLICATION_JSON)
     @ApiOperation(value = "Hakee hakukohteen valinnan vaiheiden tulokset", response = ValinnanvaiheDTO.class)
     public List<ValintatietoValinnanvaiheDTO> hakukohde(
@@ -38,7 +78,7 @@ public class ValintalaskentakoostepalveluResourceImpl {
     }
 
     @POST
-    @Path("{hakukohdeoid}/valinnanvaihe")
+    @Path("/hakukohde/{hakukohdeoid}/valinnanvaihe")
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
     @ApiOperation(value = "Lisää tuloksia valinnanvaiheelle", response = ValinnanvaiheDTO.class)
