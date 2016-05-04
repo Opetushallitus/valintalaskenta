16d3f6c424da0749dce878ee60e1bc6451288286
jussi.jartamo@gofore.com <jussi.jartamo@gofore.com>
2016-05-04 15:03:54 +0300

    assume throws exception

--- valintalaskenta-laskenta-service/src/test/java/fi/vm/sade/valintalaskenta/laskenta/service/ValintakoelaskentaSuorittajaServiceTest.java
+++ valintalaskenta-laskenta-service/src/test/java/fi/vm/sade/valintalaskenta/laskenta/service/ValintakoelaskentaSuorittajaServiceTest.java
@@ -24,6 +24,7 @@ import fi.vm.sade.service.valintaperusteet.laskenta.api.Hakemus;
 import fi.vm.sade.service.valintaperusteet.model.Funktiokutsu;
 import fi.vm.sade.valintalaskenta.domain.dto.HakemusDTO;
 import fi.vm.sade.valintalaskenta.domain.dto.HakukohdeDTO;
+import fi.vm.sade.valintalaskenta.laskenta.service.exception.LaskentaEdellinenValinnanvaiheLaskemattaException;
 import fi.vm.sade.valintalaskenta.laskenta.service.impl.conversion.HakemusDTOToHakemusConverter;
 import fi.vm.sade.valintalaskenta.tulos.mapping.ValintalaskentaModelMapper;
 import org.hamcrest.BaseMatcher;
@@ -195,7 +196,7 @@ public class ValintakoelaskentaSuorittajaServiceTest {
 
 	}
 
-	@Test
+	@Test(expected = LaskentaEdellinenValinnanvaiheLaskemattaException.class)
 	public void testEdellistaVaihettaEiLoydy() {
 		final String hakukohdeOid1 = "hakukohdeOid1";
 
@@ -230,8 +231,6 @@ public class ValintakoelaskentaSuorittajaServiceTest {
 
 		valintakoelaskentaSuorittajaService.laske(hakemus,
                 Arrays.asList(valintaperusteet1), uuid);
-		verify(valintakoeOsallistuminenDAOMock, times(0)).createOrUpdate(
-				any(ValintakoeOsallistuminen.class));
 
 	}
 
