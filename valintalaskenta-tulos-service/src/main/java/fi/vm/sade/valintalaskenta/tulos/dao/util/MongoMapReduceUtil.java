83c83caa915b29c449943eac91eb5d744b25e7ef
Jessika Penttinen <jessika.penttinen@siili.com>
2017-08-18 09:06:00 +0300

    Poistettu ylimääräisiä kommentteja ja konffeja ja luokkia

--- valintalaskenta-tulos-service/src/main/java/fi/vm/sade/valintalaskenta/tulos/dao/util/MongoMapReduceUtil.java
+++
@@ -1,5 +0,0 @@
-package fi.vm.sade.valintalaskenta.tulos.dao.util;
-
-public class MongoMapReduceUtil {
-    public static final String shallowCloneJs = "function () { function shallowClone(obj) { var copy; if (null == obj || 'object' != typeof obj || obj instanceof ObjectId || obj instanceof NumberLong || obj instanceof Date) return obj; if (obj instanceof Array) { copy = []; for (var i = 0, len = obj.length; i < len; i++) { copy[i] = obj[i]; } return copy; } if (obj instanceof Object) { copy = {}; for (var attr in obj) { if (obj.hasOwnProperty(attr)) copy[attr] = obj[attr]; } return copy; } throw new Error('Unable to copy obj'); } var copy = shallowClone(this);";
-}
