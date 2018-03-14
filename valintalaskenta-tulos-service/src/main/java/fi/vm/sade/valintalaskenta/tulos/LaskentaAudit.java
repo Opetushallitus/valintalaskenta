a1fcbe9f6f4b8e10a59a1d833df322b656db3178
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2018-03-14 13:08:43 +0200

    OY-117 : Remove duplication
    No functional changes.

--- valintalaskenta-tulos-service/src/main/java/fi/vm/sade/valintalaskenta/tulos/LaskentaAudit.java
+++ valintalaskenta-tulos-service/src/main/java/fi/vm/sade/valintalaskenta/tulos/LaskentaAudit.java
@@ -3,20 +3,10 @@ package fi.vm.sade.valintalaskenta.tulos;
 import fi.vm.sade.auditlog.ApplicationType;
 import fi.vm.sade.auditlog.Audit;
 import fi.vm.sade.sharedutils.AuditLogger;
-import org.springframework.security.core.context.SecurityContextHolder;
-
-import java.security.Principal;
-import java.util.Optional;
 
 /**
  * @author Jussi Jartamo
  */
 public class LaskentaAudit {
     public static final Audit AUDIT = new Audit(new AuditLogger(), "valintalaskenta-laskenta-service", ApplicationType.VIRKAILIJA);
-
-    public static String username() {
-        return Optional.ofNullable((Principal) SecurityContextHolder.getContext().getAuthentication()).orElse(
-                () -> "Kirjautumaton käyttäjä"
-        ).getName();
-    }
 }
