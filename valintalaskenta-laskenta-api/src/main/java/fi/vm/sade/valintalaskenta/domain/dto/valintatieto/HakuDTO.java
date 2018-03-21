edb5017f852a6d0a3700ba789db1218603183b3a
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2018-03-21 15:06:21 +0200

    OY-177 : Encapsulate filtering
    No functional changes.

--- valintalaskenta-laskenta-api/src/main/java/fi/vm/sade/valintalaskenta/domain/dto/valintatieto/HakuDTO.java
+++ valintalaskenta-laskenta-api/src/main/java/fi/vm/sade/valintalaskenta/domain/dto/valintatieto/HakuDTO.java
@@ -1,10 +1,14 @@
 package fi.vm.sade.valintalaskenta.domain.dto.valintatieto;
 
 
+import static java.lang.Boolean.TRUE;
+import static java.util.stream.Collectors.toSet;
+
 import fi.vm.sade.valintalaskenta.domain.dto.HakukohdeDTO;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 public class HakuDTO {
     private String hakuOid;
@@ -20,12 +24,23 @@ public class HakuDTO {
 
     public List<HakukohdeDTO> getHakukohteet() {
         if(hakukohteet == null) {
-            hakukohteet = new ArrayList<HakukohdeDTO>();
+            hakukohteet = new ArrayList<>();
         }
         return hakukohteet;
     }
 
     public void setHakukohteet(List<HakukohdeDTO> hakukohteet) {
         this.hakukohteet = hakukohteet;
+    }
+
+    public Set<String> filtteroiOiditHakukohteilleJoillaOnAktiivisiaJonoja() {
+        return hakukohteet.stream()
+            // Joku valinnanvaihe jossa aktiivinen jono
+            .filter(hakukohde ->
+                hakukohde.getValinnanvaihe().stream()
+                    .anyMatch(v -> v.getValintatapajonot().stream()
+                        .anyMatch(j -> TRUE.equals(j.getAktiivinen()))))
+            .map(HakukohdeDTO::getOid)
+            .collect(toSet());
     }
 }
