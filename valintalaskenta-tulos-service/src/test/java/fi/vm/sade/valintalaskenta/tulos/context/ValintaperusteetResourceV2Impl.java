c7d2c863f29d99ce74fabb8810694f94faf1d098
Tatu Honkanen <tatu.honkanen@cybercom.com>
2018-10-08 12:27:37 +0300

    OY-117 : WIP - Fix parameters.

--- valintalaskenta-tulos-service/src/test/java/fi/vm/sade/valintalaskenta/tulos/context/ValintaperusteetResourceV2Impl.java
+++ valintalaskenta-tulos-service/src/test/java/fi/vm/sade/valintalaskenta/tulos/context/ValintaperusteetResourceV2Impl.java
@@ -5,8 +5,12 @@ import fi.vm.sade.service.valintaperusteet.dto.ValintaperusteetDTO;
 import fi.vm.sade.service.valintaperusteet.dto.ValintaperusteetHakijaryhmaDTO;
 import fi.vm.sade.service.valintaperusteet.dto.ValintatapajonoDTO;
 import fi.vm.sade.service.valintaperusteet.resource.ValintaperusteetResourceV2;
+import io.swagger.annotations.ApiParam;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import java.util.ArrayList;
@@ -47,7 +51,10 @@ public class ValintaperusteetResourceV2Impl implements ValintaperusteetResourceV
     }
 
     @Override
-    public ValintatapajonoDTO updateAutomaattinenSijoitteluunSiirto(String oid, Boolean arvo, @Context HttpServletRequest request) {
+    @Path("/{valintatapajonoOid}/automaattinenSiirto")
+    public ValintatapajonoDTO updateAutomaattinenSijoitteluunSiirto(@ApiParam(value = "Valintatapajonon OID", required = true) @PathParam("valintatapajonoOid") String valintatapajonoOid,
+                                                                    @ApiParam(value = "Sijoittelustatus", required = true) @QueryParam("status") boolean status,
+                                                                    @Context HttpServletRequest request) {
         return new ValintatapajonoDTO();
     }
 }
