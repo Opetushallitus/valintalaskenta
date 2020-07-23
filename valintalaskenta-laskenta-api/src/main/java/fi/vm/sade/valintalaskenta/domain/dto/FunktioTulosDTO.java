a135d3f61f96a0932e9f9bfc4f5e0a78a0ee8a39
oph-petar <petar@phz.fi>
2020-07-23 10:06:05 +0300

    Use intern() to deduplicate strings
    For classes that appear to have the most of duplicate strings.
    Make string deduplication null safe

--- valintalaskenta-laskenta-api/src/main/java/fi/vm/sade/valintalaskenta/domain/dto/FunktioTulosDTO.java
+++ valintalaskenta-laskenta-api/src/main/java/fi/vm/sade/valintalaskenta/domain/dto/FunktioTulosDTO.java
@@ -5,76 +5,76 @@ import io.swagger.annotations.ApiModelProperty;
 
 @ApiModel(value = "FunktioTulosDTO", description = "Laskennassa saatu funktio tulos")
 public class FunktioTulosDTO {
-  @ApiModelProperty(value = "Tunniste", required = true)
-  private String tunniste;
+    @ApiModelProperty(value = "Tunniste", required = true)
+    private String tunniste;
 
-  @ApiModelProperty(value = "Varsinainen arvo, joka laskennassa on saatu", required = true)
-  private String arvo;
+    @ApiModelProperty(value = "Varsinainen arvo, joka laskennassa on saatu", required = true)
+    private String arvo;
 
-  @ApiModelProperty(value = "Suomenkielinen nimi", required = false)
-  private String nimiFi;
+    @ApiModelProperty(value = "Suomenkielinen nimi", required = false)
+    private String nimiFi;
 
-  @ApiModelProperty(value = "Ruotsinkielinen nimi", required = false)
-  private String nimiSv;
+    @ApiModelProperty(value = "Ruotsinkielinen nimi", required = false)
+    private String nimiSv;
 
-  @ApiModelProperty(value = "Englanninkielinen nimi", required = false)
-  private String nimiEn;
+    @ApiModelProperty(value = "Englanninkielinen nimi", required = false)
+    private String nimiEn;
 
-  @ApiModelProperty(value = "Näytetäänkö Omassa Opintopolussa", required = false)
-  private boolean omaopintopolku;
+    @ApiModelProperty(value = "Näytetäänkö Omassa Opintopolussa", required = false)
+    private boolean omaopintopolku;
 
-  public FunktioTulosDTO() {}
+    public FunktioTulosDTO() {}
 
-  public FunktioTulosDTO(String tunniste, String arvo) {
-    this.tunniste = tunniste;
-    this.arvo = arvo;
-  }
+    public FunktioTulosDTO(String tunniste, String arvo) {
+        this.tunniste = tunniste != null ? tunniste.intern() : null;
+        this.arvo = arvo != null ? arvo.intern() : null;
+    }
 
-  public String getTunniste() {
-    return tunniste;
-  }
+    public String getTunniste() {
+        return tunniste;
+    }
 
-  public void setTunniste(String tunniste) {
-    this.tunniste = tunniste;
-  }
+    public void setTunniste(String tunniste) {
+        this.tunniste = tunniste != null ? tunniste.intern() : null;
+    }
 
-  public String getArvo() {
-    return arvo;
-  }
+    public String getArvo() {
+        return arvo;
+    }
 
-  public void setArvo(String arvo) {
-    this.arvo = arvo;
-  }
+    public void setArvo(String arvo) {
+        this.arvo = arvo != null ? arvo.intern() : null;
+    }
 
-  public String getNimiFi() {
-    return nimiFi;
-  }
+    public String getNimiFi() {
+        return nimiFi;
+    }
 
-  public void setNimiFi(String nimiFi) {
-    this.nimiFi = nimiFi;
-  }
+    public void setNimiFi(String nimiFi) {
+        this.nimiFi = nimiFi != null ? nimiFi.intern() : null;
+    }
 
-  public String getNimiSv() {
-    return nimiSv;
-  }
+    public String getNimiSv() {
+        return nimiSv;
+    }
 
-  public void setNimiSv(String nimiSv) {
-    this.nimiSv = nimiSv;
-  }
+    public void setNimiSv(String nimiSv) {
+        this.nimiSv = nimiSv != null ? nimiSv.intern() : null;
+    }
 
-  public String getNimiEn() {
-    return nimiEn;
-  }
+    public String getNimiEn() {
+        return nimiEn;
+    }
 
-  public void setNimiEn(String nimiEn) {
-    this.nimiEn = nimiEn;
-  }
+    public void setNimiEn(String nimiEn) {
+        this.nimiEn = nimiEn != null ? nimiEn.intern() : null;
+    }
 
-  public boolean isOmaopintopolku() {
-    return omaopintopolku;
-  }
+    public boolean isOmaopintopolku() {
+        return omaopintopolku;
+    }
 
-  public void setOmaopintopolku(boolean omaopintopolku) {
-    this.omaopintopolku = omaopintopolku;
-  }
+    public void setOmaopintopolku(boolean omaopintopolku) {
+        this.omaopintopolku = omaopintopolku;
+    }
 }
