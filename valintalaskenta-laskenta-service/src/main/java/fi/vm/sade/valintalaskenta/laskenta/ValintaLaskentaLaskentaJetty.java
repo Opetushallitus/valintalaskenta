e7d600ce94e6d79063f8ea5446637d3571a8d8f4
August Kilponen <august.kilponen@gmail.com>
2022-05-24 15:48:19 +0300

    Lisätty Jettyn threadien määrää

--- valintalaskenta-laskenta-service/src/main/java/fi/vm/sade/valintalaskenta/laskenta/ValintaLaskentaLaskentaJetty.java
+++ valintalaskenta-laskenta-service/src/main/java/fi/vm/sade/valintalaskenta/laskenta/ValintaLaskentaLaskentaJetty.java
@@ -1,12 +1,13 @@
 package fi.vm.sade.valintalaskenta.laskenta;
 
 import fi.vm.sade.jetty.OpintopolkuJetty;
+import java.time.Duration;
 
 public class ValintaLaskentaLaskentaJetty extends OpintopolkuJetty {
   public static final ValintaLaskentaLaskentaJetty JETTY = new ValintaLaskentaLaskentaJetty();
   public static final String CONTEXT_PATH = "/valintalaskenta-laskenta-service";
 
   public static void main(String[] args) {
-    JETTY.start(CONTEXT_PATH);
+    JETTY.start(CONTEXT_PATH, 8080, 50, 200, Duration.ofMinutes(1L), Duration.ofSeconds(4000L));
   }
 }
