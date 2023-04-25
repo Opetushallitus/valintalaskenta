ad458ad5c6221fe2ba3f35a65a5164f01ea64ea3
Tomi Katajanheimo <tomi@katajanheimo.com>
2023-04-25 15:32:45 +0300

    Try to upgrade embedded mongo

--- valintalaskenta-laskenta-service/src/test/java/fi/vm/sade/valintalaskenta/laskenta/testing/ValintalaskentaMongodForTestsFactory.java
+++ valintalaskenta-laskenta-service/src/test/java/fi/vm/sade/valintalaskenta/laskenta/testing/ValintalaskentaMongodForTestsFactory.java
@@ -1,31 +1,29 @@
 package fi.vm.sade.valintalaskenta.laskenta.testing;
 
 import com.mongodb.MongoClient;
-import com.mongodb.ServerAddress;
-import de.flapdoodle.embed.mongo.MongodProcess;
 import de.flapdoodle.embed.mongo.distribution.Version;
-import de.flapdoodle.embed.mongo.tests.MongodForTestsFactory;
-import java.io.IOException;
-import java.lang.reflect.Field;
-import org.springframework.util.ReflectionUtils;
+import de.flapdoodle.embed.mongo.transitions.Mongod;
+import de.flapdoodle.embed.mongo.transitions.RunningMongodProcess;
+import de.flapdoodle.reverse.TransitionWalker;
 
 /**
  * A kludge to work around the problem that the mongo client tries to access mongodb with an
  * external interface, but the testing mongo only binds to localhost.
  */
-public class ValintalaskentaMongodForTestsFactory extends MongodForTestsFactory {
-  public ValintalaskentaMongodForTestsFactory() throws IOException {
-    super(Version.V3_4_15);
+public class ValintalaskentaMongodForTestsFactory {
+  private final TransitionWalker.ReachedState<RunningMongodProcess> mongoProcess;
+
+  public ValintalaskentaMongodForTestsFactory() {
+    mongoProcess = Mongod.builder().build().start(Version.Main.V3_6);
   }
 
-  @Override
+  public void shutdown() {
+    if (mongoProcess != null) {
+      mongoProcess.close();
+    }
+  }
+
   public MongoClient newMongo() {
-    Field mongoDprocessField =
-        ReflectionUtils.findField(MongodForTestsFactory.class, "mongodProcess");
-    mongoDprocessField.setAccessible(true);
-    MongodProcess mongodProcess =
-        (MongodProcess) ReflectionUtils.getField(mongoDprocessField, this);
-    return new MongoClient(
-        new ServerAddress("127.0.0.1", mongodProcess.getConfig().net().getPort()));
+    return new MongoClient("127.0.0.1", mongoProcess.current().getServerAddress().getPort());
   }
 }
--- valintalaskenta-tulos-service/src/test/java/fi/vm/sade/valintalaskenta/tulos/context/MongoConfigurationImpl.java
+++ valintalaskenta-tulos-service/src/test/java/fi/vm/sade/valintalaskenta/tulos/context/MongoConfigurationImpl.java
@@ -1,73 +1,34 @@
 package fi.vm.sade.valintalaskenta.tulos.context;
 
 import com.mongodb.MongoClient;
-import com.mongodb.ServerAddress;
-import de.flapdoodle.embed.mongo.MongodExecutable;
-import de.flapdoodle.embed.mongo.MongodProcess;
-import de.flapdoodle.embed.mongo.MongodStarter;
-import de.flapdoodle.embed.mongo.config.IMongodConfig;
-import de.flapdoodle.embed.mongo.config.MongodConfigBuilder;
-import de.flapdoodle.embed.mongo.config.Net;
 import de.flapdoodle.embed.mongo.distribution.Version;
-import de.flapdoodle.embed.process.runtime.Network;
-import java.io.IOException;
-import java.util.Random;
+import de.flapdoodle.embed.mongo.transitions.Mongod;
+import de.flapdoodle.embed.mongo.transitions.RunningMongodProcess;
+import de.flapdoodle.reverse.TransitionWalker;
 import org.mongodb.morphia.Datastore;
 import org.mongodb.morphia.Morphia;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /** @author Jussi Jartamo */
 @Configuration
 public class MongoConfigurationImpl {
-
-  private static final Logger LOG = LoggerFactory.getLogger(MongoConfigurationImpl.class);
-
   public static final String DATABASE_NAME = "test";
 
-  static final int PORT = freePort();
-
-  private static int freePort() {
-    for (int i = 0; i < 10; ++i) {
-      try {
-        return Network.getFreeServerPort();
-      } catch (IOException e) {
-      }
-    }
-    return 32452 - new Random().nextInt(20000);
+  @Bean
+  public Mongod mongoExecutable() {
+    return Mongod.builder().build();
   }
 
-  // fake mongo db
-  @Bean(destroyMethod = "stop")
-  public MongodExecutable getMongodExecutable() throws IOException {
-    IMongodConfig mongodConfig =
-        new MongodConfigBuilder()
-            .version(Version.Main.PRODUCTION)
-            .net(new Net(PORT, Network.localhostIsIPv6()))
-            .build();
-
-    MongodStarter runtime = MongodStarter.getDefaultInstance();
-
-    MongodExecutable mongodExecutable = null;
-    mongodExecutable = runtime.prepare(mongodConfig);
-
-    return mongodExecutable; // .newMongo();
-  }
-
-  @Bean(destroyMethod = "stop")
-  public MongodProcess getMongoProcess(MongodExecutable mongodExecutable) throws IOException {
-    return mongodExecutable.start();
+  @Bean(destroyMethod = "close")
+  public TransitionWalker.ReachedState<RunningMongodProcess> mongodProcess(Mongod mongoExecutable) {
+    return mongoExecutable.start(Version.Main.V3_6);
   }
 
   @Bean
-  public MongoClient getMongo(MongodProcess process) throws IOException {
-
-    // MongoClient mongo =
-    // return new MongoClient(new ServerAddress(Network.getLocalHost(),
-    // Network.getFreeServerPort()));
-    return new MongoClient(new ServerAddress("127.0.0.1", PORT)); // factory.newMongo();
+  public MongoClient mongoClient(
+      TransitionWalker.ReachedState<RunningMongodProcess> mongodProcess) {
+    return new MongoClient("127.0.0.1", mongodProcess.current().getServerAddress().getPort());
   }
 
   @Bean(name = "morphia2")
