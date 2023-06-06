3695e897c7364aea7380fd2c65c47a3899c951db
Tomi Katajanheimo <tomi@katajanheimo.com>
2023-06-06 17:50:19 +0300

    Adjust logging

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@ cd valintalaskenta-laskenta-service/
 mvn clean install
 cd ..
 cd valintalaskenta-ui/
-MAVEN_OPTS="-Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,address=8000,server=y,suspend=n" mvn clean install org.mortbay.jetty:maven-jetty-plugin:run -Pstartstack
+MAVEN_OPTS="-Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,address=8000,server=y,suspend=n" mvn clean install spring-boot:run -Pstartstack
 
