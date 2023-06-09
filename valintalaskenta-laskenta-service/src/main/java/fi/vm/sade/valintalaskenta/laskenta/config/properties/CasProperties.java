066fe21dffec232da33bc2322559431338e02793
Tomi Katajanheimo <tomi@katajanheimo.com>
2023-06-09 15:07:40 +0300

    Migrate properties into yml

--- valintalaskenta-laskenta-service/src/main/java/fi/vm/sade/valintalaskenta/laskenta/config/properties/CasProperties.java
+++ valintalaskenta-laskenta-service/src/main/java/fi/vm/sade/valintalaskenta/laskenta/config/properties/CasProperties.java
@@ -4,7 +4,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConfigurationProperties(prefix = "cas")
+@ConfigurationProperties(prefix = "cas-service")
 public class CasProperties {
   private String service;
   private Boolean sendRenew;
