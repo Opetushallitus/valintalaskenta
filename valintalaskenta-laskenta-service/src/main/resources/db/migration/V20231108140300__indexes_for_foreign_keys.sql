60aaba8607602bf71fd1d8cc3144ac41f2161920
Risto Salama <risto.salama@gofore.com>
2023-11-08 15:22:11 +0200

    Add IF NOT EXIST to migration

--- valintalaskenta-laskenta-service/src/main/resources/db/migration/V20231108140300__indexes_for_foreign_keys.sql
+++ valintalaskenta-laskenta-service/src/main/resources/db/migration/V20231108140300__indexes_for_foreign_keys.sql
@@ -1,19 +1,19 @@
-CREATE INDEX jonosija_valintatapajono ON jonosija(valintatapajono);
+CREATE INDEX IF NOT EXISTS jonosija_valintatapajono ON jonosija(valintatapajono);
 
-CREATE INDEX jonosija_hakijaryhma ON jonosija(hakijaryhma);
+CREATE INDEX IF NOT EXISTS jonosija_hakijaryhma ON jonosija(hakijaryhma);
 
-CREATE INDEX muokattu_jonosija_valintatapajono ON muokattu_jonosija(valintatapajono_oid);
+CREATE INDEX IF NOT EXISTS muokattu_jonosija_valintatapajono ON muokattu_jonosija(valintatapajono_oid);
 
-CREATE INDEX jarjestyskriteeritulos_jonosija on jarjestyskriteeritulos(jonosija);
+CREATE INDEX IF NOT EXISTS jarjestyskriteeritulos_jonosija on jarjestyskriteeritulos(jonosija);
 
-CREATE INDEX jarjestyskriteeritulos_muokattu_jonosija on jarjestyskriteeritulos(muokattu_jonosija);
+CREATE INDEX IF NOT EXISTS jarjestyskriteeritulos_muokattu_jonosija on jarjestyskriteeritulos(muokattu_jonosija);
 
-CREATE INDEX valintatapajono_valinnanvaihe on valintatapajono(valinnanvaihe);
+CREATE INDEX IF NOT EXISTS valintatapajono_valinnanvaihe on valintatapajono(valinnanvaihe);
 
-CREATE INDEX valintakoe_valintakoe_valinnanvaihe ON valintakoe(valintakoe_valinnanvaihe);
+CREATE INDEX IF NOT EXISTS valintakoe_valintakoe_valinnanvaihe ON valintakoe(valintakoe_valinnanvaihe);
 
-CREATE INDEX valintakoe_valinnanvaihe_hakutoive on valintakoe_valinnanvaihe(hakutoive);
+CREATE INDEX IF NOT EXISTS valintakoe_valinnanvaihe_hakutoive on valintakoe_valinnanvaihe(hakutoive);
 
-CREATE INDEX hakutoive_valintakoe_osallistuminen on hakutoive(valintakoe_osallistuminen);
+CREATE INDEX IF NOT EXISTS hakutoive_valintakoe_osallistuminen on hakutoive(valintakoe_osallistuminen);
 
 
