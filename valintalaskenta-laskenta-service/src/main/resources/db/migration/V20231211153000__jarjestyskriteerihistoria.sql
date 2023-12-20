49717c1d7780a6f088e8b21eb890c3692ab7ca7c
Risto Salama <risto.salama@gofore.com>
2023-12-20 14:32:45 +0200

    Allow historia to be null

--- valintalaskenta-laskenta-service/src/main/resources/db/migration/V20231211153000__jarjestyskriteerihistoria.sql
+++ valintalaskenta-laskenta-service/src/main/resources/db/migration/V20231211153000__jarjestyskriteerihistoria.sql
@@ -2,6 +2,6 @@ CREATE TABLE jarjestyskriteerihistoria (
     id BIGSERIAL PRIMARY KEY,
     tunniste uuid NOT NULL DEFAULT uuid_generate_v4(),
     created_at timestamp with time zone default now(),
-    historia text NOT NULL,
+    historia text,
     laskettu_uudelleen boolean NOT NULL DEFAULT false
 );
