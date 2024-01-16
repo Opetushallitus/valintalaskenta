c20e0af85bde36b9f682409e8b5e2414c9419325
Risto Salama <risto.salama@gofore.com>
2024-01-16 15:27:29 +0200

    Update status to migration control table

--- mongo-to-postgres/src/migration-control.js
+++ mongo-to-postgres/src/migration-control.js
@@ -1,13 +1,14 @@
 import {getDistinctHakukohteetAndHaut} from "./get-from-mongo.js";
 
-const AMOUNT_TO_FETCH_AT_ONCE = 3;
+const AMOUNT_TO_FETCH_AT_ONCE = 10;
+const CONTROL_TABLE = 'data_migration_control';
 
 const populateControlTable = async (knex, mongoConn) => {
   const result = await getDistinctHakukohteetAndHaut(mongoConn);
 
   await knex.transaction(async trx => {
     for (const obj of result) {
-      await trx('data_migration_control')
+      await trx(CONTROL_TABLE)
         .insert({hakukohde_oid : obj.hakukohde, haku_oid : obj.haku});
     }
   });
@@ -16,7 +17,7 @@ const populateControlTable = async (knex, mongoConn) => {
 
 export const fetchFromMigrationControl = async (knex, mongoConn) => {
   
-  const populated = await knex('data_migration_control')
+  const populated = await knex(CONTROL_TABLE)
     .select('id')
     .limit(1)
 
@@ -26,7 +27,7 @@ export const fetchFromMigrationControl = async (knex, mongoConn) => {
     await populateControlTable(knex, mongoConn);
   }
   
-  const data = await knex('data_migration_control')
+  const data = await knex(CONTROL_TABLE)
     .select('haku_oid', 'hakukohde_oid')
     .whereNull('success')
     .orderBy('id', 'asc')
@@ -35,5 +36,16 @@ export const fetchFromMigrationControl = async (knex, mongoConn) => {
   return data.map(d => {
     return {haku: d.haku_oid, hakukohde: d.hakukohde_oid};
   });
+}
+
+export const updateMigrationRow = async (knex, oidObj, isSuccess, error) => {
+  const isHaku = oidObj.hakukohde == null;
+  const oid = isHaku? oidObj.haku : oidObj.hakukohde;
+  console.log(`updateMigrationRow, ${oid}, ${isHaku}, ${isSuccess}`);
+  await knex(CONTROL_TABLE)
+    .where(isHaku? 'haku_oid' : 'hakukohde_oid', '=', oid)
+    .update({ updated_at: new Date().toUTCString(),
+              success: isSuccess,
+              error_message: error});
 }
 
