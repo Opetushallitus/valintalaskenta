47057cd2f06f39b1dacb6c67c6285af2cf1df62d
Santeri Korri <santeri.korri@knowit.fi>
2024-04-30 09:38:50 +0300

    OK-478 Proxytetaan laskennan seuranta valintalaskennan kautta

---
+++ valintalaskenta-laskenta-service/src/main/java/fi/vm/sade/valintalaskenta/laskenta/resource/dto/seuranta/LaskentaTila.java
@@ -0,0 +1,8 @@
+package fi.vm.sade.valintalaskenta.laskenta.resource.dto.seuranta;
+
+public enum LaskentaTila {
+  ALOITTAMATTA,
+  VALMIS,
+  PERUUTETTU,
+  MENEILLAAN
+}
---
+++ valintalaskenta-laskenta-service/src/main/java/fi/vm/sade/valintalaskenta/laskenta/resource/dto/seuranta/LaskentaTyyppi.java
@@ -0,0 +1,7 @@
+package fi.vm.sade.valintalaskenta.laskenta.resource.dto.seuranta;
+
+public enum LaskentaTyyppi {
+  VALINTARYHMA,
+  HAKU,
+  HAKUKOHDE
+}
