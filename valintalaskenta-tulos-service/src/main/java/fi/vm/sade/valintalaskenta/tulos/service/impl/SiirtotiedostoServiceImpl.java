5c7639cd6781596e4743c82d10f7c9eb022bc0ae
August Kilponen <august.kilponen@gmail.com>
2024-07-09 15:29:48 +0300

    Mahdollistettu siirtotiedostojen beanin disablointi

--- valintalaskenta-tulos-service/src/main/java/fi/vm/sade/valintalaskenta/tulos/service/impl/SiirtotiedostoServiceImpl.java
+++ valintalaskenta-tulos-service/src/main/java/fi/vm/sade/valintalaskenta/tulos/service/impl/SiirtotiedostoServiceImpl.java
@@ -21,9 +21,11 @@ import java.util.stream.Collectors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Service;
 
 @Service
+@ConditionalOnProperty("valintalaskenta.enable-siirtotiedosto-service")
 public class SiirtotiedostoServiceImpl implements SiirtotiedostoService {
   private static final Logger LOGGER = LoggerFactory.getLogger(SiirtotiedostoServiceImpl.class);
 
