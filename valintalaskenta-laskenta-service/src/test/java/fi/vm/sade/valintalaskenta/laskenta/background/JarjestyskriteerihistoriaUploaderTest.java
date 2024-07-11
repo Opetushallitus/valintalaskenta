b34b408816254bbcaec866f561375c0f623431ab
Risto Salama <risto.salama@gofore.com>
2024-07-11 17:25:13 +0300

    Fix test

--- valintalaskenta-laskenta-service/src/test/java/fi/vm/sade/valintalaskenta/laskenta/background/JarjestyskriteerihistoriaUploaderTest.java
+++ valintalaskenta-laskenta-service/src/test/java/fi/vm/sade/valintalaskenta/laskenta/background/JarjestyskriteerihistoriaUploaderTest.java
@@ -1,7 +1,6 @@
 package fi.vm.sade.valintalaskenta.laskenta.background;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -34,7 +33,7 @@ public class JarjestyskriteerihistoriaUploaderTest {
     when(dao.fetchOldest()).thenReturn(List.of(createHistoria(false)));
     uploader.moveJarjestyskriteeriHistoriaFromDatabaseToS3();
     verify(dokumenttipalvelu)
-        .save(anyString(), anyString(), any(), anyString(), any(InputStream.class));
+        .save(anyString(), anyString(), any(), anyString(), any(InputStream.class), eq(true));
     verify(dao).delete(any(Long.class));
   }
 
