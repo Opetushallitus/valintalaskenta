228ad9e2d5e406f488c60686c0aa737608fb8b17
Santeri Korri <santeri.korri@knowit.fi>
2024-09-12 08:07:49 +0300

    OK-611 Poistettu käyttämättömiä luokkia, siiretty paketista toiseen

--- valintalaskenta-laskenta-service/src/main/java/fi/vm/sade/valinta/kooste/external/resource/kouta/impl/KoutaAsyncResourceImpl.java
+++ valintalaskenta-laskenta-service/src/main/java/fi/vm/sade/valinta/kooste/external/resource/kouta/impl/KoutaAsyncResourceImpl.java
@@ -1,11 +1,11 @@
 package fi.vm.sade.valinta.kooste.external.resource.kouta.impl;
 
 import com.google.gson.reflect.TypeToken;
+import fi.vm.sade.valinta.kooste.external.resource.UrlConfiguration;
 import fi.vm.sade.valinta.kooste.external.resource.kouta.KoutaAsyncResource;
 import fi.vm.sade.valinta.kooste.external.resource.kouta.KoutaHakukohde;
 import fi.vm.sade.valinta.kooste.external.resource.kouta.dto.KoutaHakukohdeDTO;
 import fi.vm.sade.valinta.kooste.external.resource.viestintapalvelu.RestCasClient;
-import fi.vm.sade.valinta.kooste.url.UrlConfiguration;
 import java.util.Collections;
 import java.util.concurrent.CompletableFuture;
 import org.springframework.beans.factory.annotation.Autowired;
