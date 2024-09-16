e8b55f5230c8ea6a2b5a93ec6f7108d8c7874398
Santeri Korri <santeri.korri@knowit.fi>
2024-09-16 16:40:56 +0300

    OK-611 Poistettu ValintaLaskentaAsyncResource

--- valintalaskenta-laskenta-service/src/main/java/fi/vm/sade/valintalaskenta/laskenta/resource/ValintalaskentaResourceImpl.java
+++ valintalaskenta-laskenta-service/src/main/java/fi/vm/sade/valintalaskenta/laskenta/resource/ValintalaskentaResourceImpl.java
@@ -80,7 +80,7 @@ public class ValintalaskentaResourceImpl {
     }
   }
 
-  public String laske(@RequestBody final Laskentakutsu laskentakutsu) throws Exception {
+  public String laske(final Laskentakutsu laskentakutsu) {
     try {
       String pollKey = laskentakutsu.getPollKey();
       laskentakutsu.populoiSuoritustiedotLaskeDtoille();
@@ -105,7 +105,7 @@ public class ValintalaskentaResourceImpl {
     }
   }
 
-  public String valintakokeet(@RequestBody final Laskentakutsu laskentakutsu) throws Exception {
+  public String valintakokeet(final Laskentakutsu laskentakutsu) {
     try {
       String pollKey = laskentakutsu.getPollKey();
       laskentakutsu.populoiSuoritustiedotLaskeDtoille();
@@ -131,7 +131,7 @@ public class ValintalaskentaResourceImpl {
     }
   }
 
-  public String laskeKaikki(@RequestBody final Laskentakutsu laskentakutsu) throws Exception {
+  public String laskeKaikki(final Laskentakutsu laskentakutsu) {
     try {
       String pollKey = laskentakutsu.getPollKey();
       laskentakutsu.populoiSuoritustiedotLaskeDtoille();
