e522482195a60bfedc16f4028f774eeaa341622b
Santeri Korri <santeri.korri@knowit.fi>
2024-09-18 14:29:11 +0300

    OK-611 parametrisoidaan host.virkailija

--- valintalaskenta-laskenta-service/src/test/java/fi/vm/sade/valintalaskenta/laskenta/DevApp.java
+++ valintalaskenta-laskenta-service/src/test/java/fi/vm/sade/valintalaskenta/laskenta/DevApp.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Profile;
 @Profile("dev")
 public class DevApp {
 
+  private static final String ENVIRONMENT = "hahtuva";
+
   public static void main(String[] args) {
     System.setProperty("spring.profiles.active", "dev");
 
@@ -16,15 +18,15 @@ public class DevApp {
     System.setProperty("valintalaskenta-laskenta-service.postgresql.password", "password");
     System.setProperty("valintalaskenta-laskenta-service.postgresql.password", "password");
 
-    System.setProperty("host.virkailija", "virkailija.untuvaopintopolku.fi");
-    System.setProperty("host.cas", "virkailija.untuvaopintopolku.fi");
+    System.setProperty("host.virkailija", String.format("virkailija.%sopintopolku.fi", ENVIRONMENT));
+    System.setProperty("host.cas", String.format("virkailija.%sopintopolku.fi", ENVIRONMENT));
     System.setProperty("host.alb", "http://localhost:8888");
-    System.setProperty("host.host-virkailija", "virkailija.untuvaopintopolku.fi");
+    System.setProperty("host.host-virkailija", String.format("virkailija.%sopintopolku.fi", ENVIRONMENT));
     System.setProperty("host.host-alb", "http://localhost:8888");
 
     System.setProperty(
         "cas-service.service",
-        "https://virkailija.untuvaopintopolku.fi/valintalaskenta-laskenta-service");
+        String.format("https://virkailija.%sopintopolku.fi/valintalaskenta-laskenta-service", ENVIRONMENT));
 
     System.setProperty("server.servlet.context-path", App.CONTEXT_PATH);
 
