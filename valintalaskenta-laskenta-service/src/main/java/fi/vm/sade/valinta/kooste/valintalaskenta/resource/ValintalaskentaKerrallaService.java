5f698f45f0efe93155d7b9c39d1f8e06df1f89b9
Santeri Korri <santeri.korri@knowit.fi>
2024-09-18 12:37:19 +0300

    OK-611 Haetaan ja autorisoidaan hakukohteet jo ValintaLaskentaKerrallaResource-luokassa
      - näin autorisointi tapahtuu kokonaisuudessaan yhdessä kerroksessa

--- valintalaskenta-laskenta-service/src/main/java/fi/vm/sade/valinta/kooste/valintalaskenta/resource/ValintalaskentaKerrallaService.java
+++ valintalaskenta-laskenta-service/src/main/java/fi/vm/sade/valinta/kooste/valintalaskenta/resource/ValintalaskentaKerrallaService.java
@@ -3,8 +3,6 @@ package fi.vm.sade.valinta.kooste.valintalaskenta.resource;
 import fi.vm.sade.service.valintaperusteet.dto.HakukohdeViiteDTO;
 import fi.vm.sade.valinta.kooste.dto.Vastaus;
 import fi.vm.sade.valinta.kooste.seuranta.LaskentaSeurantaService;
-import fi.vm.sade.valinta.kooste.external.resource.valintaperusteet.ValintaperusteetAsyncResource;
-import fi.vm.sade.valinta.kooste.security.HakukohdeOIDAuthorityCheck;
 import fi.vm.sade.valinta.kooste.valintalaskenta.actor.dto.HakukohdeJaOrganisaatio;
 import fi.vm.sade.valinta.kooste.valintalaskenta.dto.Laskenta;
 import fi.vm.sade.valinta.kooste.valintalaskenta.dto.LaskentaInfo;
@@ -12,22 +10,17 @@ import fi.vm.sade.valinta.kooste.valintalaskenta.dto.Maski;
 import fi.vm.sade.valinta.kooste.valintalaskenta.route.ValintalaskentaKerrallaRoute;
 import fi.vm.sade.valinta.kooste.valintalaskenta.route.ValintalaskentaKerrallaRouteValvomo;
 import fi.vm.sade.valintalaskenta.domain.dto.seuranta.HakukohdeDto;
-import fi.vm.sade.valintalaskenta.domain.dto.seuranta.LaskentaDto;
-import fi.vm.sade.valintalaskenta.domain.dto.seuranta.LaskentaTyyppi;
 import fi.vm.sade.valintalaskenta.domain.dto.seuranta.TunnisteDto;
-import io.reactivex.Observable;
 import java.util.Collection;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Collectors;
-import org.apache.commons.lang3.tuple.Pair;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Service;
 import org.springframework.web.context.request.async.DeferredResult;
 
@@ -36,39 +29,13 @@ public class ValintalaskentaKerrallaService {
   private static final Logger LOG = LoggerFactory.getLogger(ValintalaskentaKerrallaService.class);
 
   @Autowired private ValintalaskentaKerrallaRouteValvomo valintalaskentaValvomo;
-  @Autowired private ValintaperusteetAsyncResource valintaperusteetAsyncResource;
   @Autowired private ValintalaskentaKerrallaRoute valintalaskentaRoute;
   @Autowired private LaskentaSeurantaService laskentaSeurantaService;
 
   public ValintalaskentaKerrallaService() {}
 
-  private void autorisoiHakukohteet(String hakuOid,
-                                    Collection<HakukohdeJaOrganisaatio> haunHakukohteetOids,
-                                    Observable<HakukohdeOIDAuthorityCheck> authCheck) {
-    authCheck
-        .blockingNext()
-        .forEach(
-            authorityCheck ->
-                haunHakukohteetOids.forEach(
-                    hk -> {
-                      if (!authorityCheck.test(hk.getHakukohdeOid())) {
-                        LOG.error(
-                            String.format(
-                                "Ei oikeutta aloittaa laskentaa hakukohteelle %s haussa %s",
-                                hk.getHakukohdeOid(), hakuOid));
-                        throw new AccessDeniedException(
-                            "Ei oikeutta aloittaa laskentaa");
-                      }
-                    }));
-  }
-
-  public TunnisteDto kaynnistaLaskentaHaulle(LaskentaParams laskentaParams) {
-    return kaynnistaLaskentaHaulle(laskentaParams, Observable.empty());
-  }
-
-  public TunnisteDto kaynnistaLaskentaHaulle(
-      LaskentaParams laskentaParams,
-      Observable<HakukohdeOIDAuthorityCheck> authCheck) {
+  public TunnisteDto kaynnistaLaskentaHaulle(LaskentaParams laskentaParams,
+                                             List<HakukohdeViiteDTO> hakukohdeViitteet) {
     String hakuOid = laskentaParams.getHakuOid();
     Optional<String> uuidForExistingNonMaskedLaskenta =
         uuidForExistingNonMaskedLaskenta(laskentaParams.getMaski(), hakuOid);
@@ -83,14 +50,9 @@ public class ValintalaskentaKerrallaService {
     }
 
     LOG.info("Aloitetaan laskenta haulle {}", hakuOid);
-    List<HakukohdeViiteDTO> hakukohdeViitteet = valintaperusteetAsyncResource.haunHakukohteet(hakuOid).blockingFirst();
     Collection<HakukohdeJaOrganisaatio> haunHakukohteetOids =
         kasitteleHakukohdeViitteet(
             hakukohdeViitteet, hakuOid, laskentaParams.getMaski());
-
-    if (!LaskentaTyyppi.VALINTARYHMA.equals(laskentaParams.getLaskentatyyppi())) {
-      this.autorisoiHakukohteet(hakuOid, haunHakukohteetOids, authCheck);
-    }
 
     final List<HakukohdeDto> hakukohdeDtos = toHakukohdeDto(haunHakukohteetOids);
     validateHakukohdeDtos(haunHakukohteetOids, hakukohdeDtos);
@@ -124,15 +86,8 @@ public class ValintalaskentaKerrallaService {
   private void resetoiTilat(String uuid, DeferredResult<ResponseEntity<Vastaus>> result) {
     laskentaSeurantaService
         .resetoiTilat(uuid)
-        .flatMap(
-            (LaskentaDto laskenta) ->
-                Observable.just(laskenta)
-                    .zipWith(
-                        valintaperusteetAsyncResource.haunHakukohteet(laskenta.getHakuOid()),
-                        Pair::of))
         .subscribe(
-            (Pair<LaskentaDto, List<HakukohdeViiteDTO>> laskentaJaHakukohdeViitteet) -> {
-              LaskentaDto laskenta = laskentaJaHakukohdeViitteet.getLeft();
+            laskenta -> {
               notifyWorkAvailable(
                   new TunnisteDto(laskenta.getUuid(), laskenta.getLuotiinkoUusiLaskenta()), result);
             },
