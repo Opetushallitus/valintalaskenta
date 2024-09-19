3e4f5bf96e24ab54c289d1bffd12566020a7dec5
Santeri Korri <santeri.korri@knowit.fi>
2024-09-19 14:21:30 +0300

    OK-611 Refaktoroitu autorisointia

--- valintalaskenta-laskenta-service/src/main/java/fi/vm/sade/valinta/kooste/security/AuthorityCheckService.java
+++ valintalaskenta-laskenta-service/src/main/java/fi/vm/sade/valinta/kooste/security/AuthorityCheckService.java
@@ -4,18 +4,19 @@ import static fi.vm.sade.valinta.kooste.util.SecurityUtil.*;
 import static java.util.concurrent.TimeUnit.MINUTES;
 
 import fi.vm.sade.valinta.kooste.external.resource.organisaatio.OrganisaatioAsyncResource;
+import fi.vm.sade.valinta.kooste.external.resource.tarjonta.Haku;
 import fi.vm.sade.valinta.kooste.external.resource.tarjonta.TarjontaAsyncResource;
 import fi.vm.sade.valinta.kooste.external.resource.valintaperusteet.ValintaperusteetAsyncResource;
-import io.reactivex.Observable;
+import fi.vm.sade.valintalaskenta.domain.dto.seuranta.LaskentaDto;
+import fi.vm.sade.valintalaskenta.domain.dto.seuranta.LaskentaTyyppi;
 import java.util.*;
 import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -26,19 +27,18 @@ public class AuthorityCheckService {
   @Autowired private OrganisaatioAsyncResource organisaatioAsyncResource;
   @Autowired private ValintaperusteetAsyncResource valintaperusteetAsyncResource;
 
-  public CompletableFuture<HakukohdeOIDAuthorityCheck> getAuthorityCheckForRoles(
-      Collection<String> roles) {
+  private HakukohdeOIDAuthorityCheck getAuthorityCheckForRoles(Collection<String> roles) {
     final Collection<String> authorities = getAuthoritiesFromAuthenticationStartingWith(roles);
     final Set<String> organizationOids = parseOrganizationOidsFromSecurityRoles(authorities);
+
     boolean isRootAuthority = organizationOids.stream().anyMatch(oid -> isRootOrganizationOID(oid));
     if (isRootAuthority) {
-      return CompletableFuture.completedFuture((oid) -> true);
+      return (oid) -> true;
     } else {
       final Set<String> organizationGroupOids =
           parseOrganizationGroupOidsFromSecurityRoles(authorities);
       if (organizationGroupOids.isEmpty() && organizationOids.isEmpty()) {
-        return CompletableFuture.failedFuture(
-            new RuntimeException("Unauthorized. User has no organization OIDS"));
+        throw new RuntimeException("Unauthorized. User has no organization OIDS");
       }
       CompletableFuture<Set<String>> searchByOrganizationOids =
           Optional.of(organizationOids)
@@ -52,10 +52,15 @@ public class AuthorityCheckService {
               .map(tarjontaAsyncResource::hakukohdeSearchByOrganizationGroupOids)
               .orElse(CompletableFuture.completedFuture(Collections.emptySet()));
 
-      return searchByOrganizationOids.thenComposeAsync(
+      CompletableFuture<HakukohdeOIDAuthorityCheck> authCheck = searchByOrganizationOids.thenComposeAsync(
           byOrgs ->
               searchByOrganizationGroupOids.thenApplyAsync(
                   byGroups -> (oid) -> byOrgs.contains(oid) || byGroups.contains(oid)));
+      try {
+        return authCheck.get();
+      } catch (Exception e) {
+        throw new RuntimeException(e);
+      }
     }
   }
 
@@ -67,16 +72,16 @@ public class AuthorityCheckService {
       return;
     }
 
-    boolean isAuthorized =
-        Observable.fromFuture(tarjontaAsyncResource.haeHaku(hakuOid))
-            .map(haku -> isAuthorizedForAnyParentOid(haku.tarjoajaOids, userRoles, requiredRoles))
-            .timeout(2, MINUTES)
-            .blockingFirst();
+    Haku haku;
+    try {
+      haku = tarjontaAsyncResource.haeHaku(hakuOid).get(2, MINUTES);
+    } catch(Exception e) {
+      throw new RuntimeException(e);
+    }
+    boolean isAuthorized = isAuthorizedForAnyParentOid(haku.tarjoajaOids, userRoles, requiredRoles);
 
     if (!isAuthorized) {
-      String msg =
-          String.format(
-              "Käyttäjällä ei oikeutta haun %s tarjoajaan tai sen yläorganisaatioihin.", hakuOid);
+      String msg = String.format("Käyttäjällä ei oikeutta haun %s tarjoajaan tai sen yläorganisaatioihin.", hakuOid);
       LOG.error(msg);
       throw new AccessDeniedException(msg);
     }
@@ -91,33 +96,13 @@ public class AuthorityCheckService {
       return;
     }
 
-    boolean isAuthorized =
-        Observable.fromFuture(getAuthorityCheckForRoles(requiredRoles))
-            .map(authorityCheck -> hakukohdeOids.stream().anyMatch(authorityCheck))
-            .timeout(2, MINUTES)
-            .blockingFirst();
-
+    HakukohdeOIDAuthorityCheck authCheck = getAuthorityCheckForRoles(requiredRoles);
+    boolean isAuthorized = hakukohdeOids.stream().anyMatch(authCheck);
     if (!isAuthorized) {
       String msg =
           String.format("Käyttäjällä ei oikeutta yhteenkään hakukohteeseen: %s", hakukohdeOids);
       LOG.error(msg);
       throw new AccessDeniedException(msg);
-    }
-  }
-
-  public HakukohdeOIDAuthorityCheck getHakukohdeOidBasedAuthCheck(
-      Context context, Collection<String> requiredRoles) {
-    setContext(context);
-    Collection<? extends GrantedAuthority> userRoles = getRoles();
-    if (containsOphRole(userRoles)) {
-      return (oid) -> true;
-    } else {
-      HakukohdeOIDAuthorityCheck authCheck =
-          Observable.fromFuture(getAuthorityCheckForRoles(requiredRoles))
-              .timeout(2, MINUTES)
-              .blockingFirst();
-      clearContext();
-      return authCheck;
     }
   }
 
@@ -189,39 +174,18 @@ public class AuthorityCheckService {
     }
   }
 
-  /**
-   * Käyttöoikeustarkastelun konteksti
-   *
-   * <p>Tämän avulla käyttöoikeustarkastelun voi siirtää käyttäjän tunnistaneesta säikeestä toiseen
-   * säikeeseen.
-   */
-  public static class Context {
-    protected final SecurityContext securityContext;
-
-    protected Context(SecurityContext securityContext) {
-      this.securityContext = securityContext;
+  public Boolean checkAuthorizationForLaskenta(LaskentaDto laskentaDto, Collection<String> requiredRoles) {
+    if (LaskentaTyyppi.HAKU.equals(laskentaDto.getTyyppi())) {
+      this.checkAuthorizationForHaku(
+          laskentaDto.getHakuOid(), requiredRoles);
+    } else {
+      final List<String> hakukohdeOids =
+          laskentaDto.getHakukohteet().stream()
+              .map(hk -> hk.getHakukohdeOid())
+              .collect(Collectors.toList());
+      this.checkAuthorizationForHakukohteet(
+          hakukohdeOids, requiredRoles);
     }
-
-    protected SecurityContext getSecurityContext() {
-      return securityContext;
-    }
-  }
-
-  public Context getContext() {
-    return new Context(SecurityContextHolder.getContext());
-  }
-
-  public void withContext(Context context, Runnable callback) {
-    setContext(context);
-    callback.run();
-    clearContext();
-  }
-
-  private void setContext(Context context) {
-    SecurityContextHolder.setContext(context.getSecurityContext());
-  }
-
-  private void clearContext() {
-    SecurityContextHolder.clearContext();
+    return Boolean.TRUE;
   }
 }
