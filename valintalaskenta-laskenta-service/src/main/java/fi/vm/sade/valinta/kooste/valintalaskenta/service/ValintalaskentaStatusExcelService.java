b27bd39c6be471fc90c3c94f22048917cea230cd
Santeri Korri <santeri.korri@knowit.fi>
2024-09-19 15:34:07 +0300

    OK-611 Uudelleennimitetty luokkkia

--- valintalaskenta-laskenta-service/src/main/java/fi/vm/sade/valinta/kooste/valintalaskenta/service/ValintalaskentaStatusExcelService.java
+++ valintalaskenta-laskenta-service/src/main/java/fi/vm/sade/valinta/kooste/valintalaskenta/service/ValintalaskentaStatusExcelService.java
@@ -1,13 +1,14 @@
-package fi.vm.sade.valinta.kooste.valintalaskenta.resource;
+package fi.vm.sade.valinta.kooste.valintalaskenta.service;
 
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import fi.vm.sade.valinta.kooste.util.ExcelExportUtil;
-import fi.vm.sade.valinta.kooste.valintalaskenta.excel.LaskentaDtoAsExcel;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
 
+import fi.vm.sade.valintalaskenta.domain.dto.seuranta.*;
 import fi.vm.sade.valintalaskenta.laskenta.dao.SeurantaDao;
 import io.reactivex.Observable;
 import org.slf4j.Logger;
@@ -19,9 +20,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.context.request.async.DeferredResult;
 
 @Service
-public class ValintalaskentaStatusExcelHandler {
+public class ValintalaskentaStatusExcelService {
   private static final Logger LOG =
-      LoggerFactory.getLogger(ValintalaskentaStatusExcelHandler.class);
+      LoggerFactory.getLogger(ValintalaskentaStatusExcelService.class);
 
   @Autowired private SeurantaDao seurantaDao;
 
@@ -44,7 +45,7 @@ public class ValintalaskentaStatusExcelHandler {
         .subscribe(
             laskenta -> {
               try {
-                byte[] bytes = LaskentaDtoAsExcel.laskentaDtoAsExcel(laskenta);
+                byte[] bytes = laskentaDtoAsExcel(laskenta);
                 result.setResult(excelResponse("yhteenveto.xls", bytes));
               } catch (Throwable e) {
                 LOG.error(
@@ -95,6 +96,54 @@ public class ValintalaskentaStatusExcelHandler {
     final Map<String, Object[][]> sheetAndGrid = Maps.newHashMap();
     sheetAndGrid.put(sheetName, grid.toArray(new Object[][] {}));
 
+    return ExcelExportUtil.exportGridSheetsAsXlsBytes(sheetAndGrid);
+  }
+
+  private static byte[] laskentaDtoAsExcel(LaskentaDto laskenta) {
+    Map<String, Object[][]> sheetAndGrid = Maps.newHashMap();
+    {
+      List<Object[]> grid = Lists.newArrayList();
+      if (laskenta.getIlmoitus() != null) {
+        IlmoitusDto ilmoitus = laskenta.getIlmoitus();
+        if (IlmoitusTyyppi.VIRHE.equals(ilmoitus.getTyyppi())) {
+          grid.add(new Object[] {"Virheilmoitus:", ilmoitus.getOtsikko()});
+        } else {
+          grid.add(new Object[] {"Ilmoitus:", ilmoitus.getOtsikko()});
+        }
+      }
+      grid.add(new Object[] {""});
+      grid.add(new Object[] {"Suorittamattomat hakukohteet"});
+      if (laskenta.getHakukohteet() != null) {
+        for (HakukohdeDto hakukohde :
+            laskenta.getHakukohteet().stream()
+                .filter(h -> !HakukohdeTila.VALMIS.equals(h.getTila()))
+                .collect(Collectors.toList())) {
+          List<String> rivi = Lists.newArrayList();
+          rivi.add(hakukohde.getHakukohdeOid());
+          if (hakukohde.getIlmoitukset() != null) {
+            rivi.addAll(
+                hakukohde.getIlmoitukset().stream()
+                    .map(i -> i.getOtsikko())
+                    .collect(Collectors.toList()));
+          }
+          grid.add(rivi.toArray());
+        }
+      }
+      sheetAndGrid.put("Kesken", grid.toArray(new Object[][] {}));
+    }
+    {
+      List<Object[]> grid = Lists.newArrayList();
+      grid.add(new Object[] {"Valmistuneet hakukohteet"});
+      if (laskenta.getHakukohteet() != null) {
+        for (HakukohdeDto hakukohde :
+            laskenta.getHakukohteet().stream()
+                .filter(h -> HakukohdeTila.VALMIS.equals(h.getTila()))
+                .collect(Collectors.toList())) {
+          grid.add(new Object[] {hakukohde.getHakukohdeOid()});
+        }
+      }
+      sheetAndGrid.put("Valmiit", grid.toArray(new Object[][] {}));
+    }
     return ExcelExportUtil.exportGridSheetsAsXlsBytes(sheetAndGrid);
   }
 }
