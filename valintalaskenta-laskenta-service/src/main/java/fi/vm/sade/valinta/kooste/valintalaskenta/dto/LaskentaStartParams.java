18dc772e95e50cc22058feaa6cf0d81dba026ab6
Santeri Korri <santeri.korri@knowit.fi>
2024-09-20 13:11:14 +0300

    OK-611 Poistettu ylimääräisiä interface-luokkia

--- valintalaskenta-laskenta-service/src/main/java/fi/vm/sade/valinta/kooste/valintalaskenta/dto/LaskentaStartParams.java
+++ valintalaskenta-laskenta-service/src/main/java/fi/vm/sade/valinta/kooste/valintalaskenta/dto/LaskentaStartParams.java
@@ -4,7 +4,7 @@ import fi.vm.sade.valinta.kooste.external.resource.valintatulosservice.dto.Audit
 import fi.vm.sade.valintalaskenta.domain.dto.seuranta.LaskentaTyyppi;
 import java.util.Collection;
 
-public class LaskentaStartParams implements LaskentaInfo {
+public class LaskentaStartParams {
   private static final String NIMI_FORMAT = "Laskenta hakuOid(%s) uuid(%s) hakukohteita(%d)";
   private final String uuid;
   private final String hakuOid;
@@ -16,27 +16,6 @@ public class LaskentaStartParams implements LaskentaInfo {
   private final Collection<HakukohdeJaOrganisaatio> hakukohdeDtos;
   private final LaskentaTyyppi tyyppi;
   private final AuditSession auditSession;
-
-  public LaskentaStartParams(
-      AuditSession auditSession,
-      String uuid,
-      String hakuOid,
-      boolean erillishaku,
-      Integer valinnanvaihe,
-      Boolean valintakoelaskenta,
-      Collection<HakukohdeJaOrganisaatio> hakukohdeDtos,
-      LaskentaTyyppi tyyppi) {
-    this.auditSession = auditSession;
-    this.uuid = uuid;
-    this.hakuOid = hakuOid;
-    this.osittainenLaskenta = false;
-    this.valinnanvaihe = valinnanvaihe;
-    this.valintakoelaskenta = valintakoelaskenta;
-    this.hakukohdeDtos = hakukohdeDtos;
-    this.valintaryhmalaskenta = false;
-    this.tyyppi = tyyppi;
-    this.erillishaku = erillishaku;
-  }
 
   public LaskentaStartParams(
       AuditSession auditSession,
