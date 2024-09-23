14eb503f3e3644c0a0f3f5c19178db01086996a4
Santeri Korri <santeri.korri@knowit.fi>
2024-09-23 08:41:09 +0300

    OK-611 Refaktoroitia, poistettu käyttämätöntä koodia

--- valintalaskenta-laskenta-service/src/main/java/fi/vm/sade/valinta/kooste/external/resource/ataru/AtaruAsyncResource.java
+++ valintalaskenta-laskenta-service/src/main/java/fi/vm/sade/valinta/kooste/external/resource/ataru/AtaruAsyncResource.java
@@ -1,24 +1,18 @@
 package fi.vm.sade.valinta.kooste.external.resource.ataru;
 
-import fi.vm.sade.valinta.kooste.external.resource.ataru.dto.AtaruHakemusPrototyyppi;
-import fi.vm.sade.valinta.kooste.external.resource.ataru.dto.AtaruSyntheticApplicationResponse;
 import fi.vm.sade.valinta.kooste.util.HakemusWrapper;
-import io.reactivex.Observable;
-import java.util.Collection;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 public interface AtaruAsyncResource {
-  CompletableFuture<List<HakemusWrapper>> getApplicationsByHakukohde(String hakukohdeOid);
 
+  /**
+   *
+   * @param hakukohdeOid                  Hakukohteen tunniste
+   * @param withHarkinnanvaraisuustieto   Sisällytetäänkö harkinnanvaraisuustieto. Harkinnanvaraisuustiedon haku on
+   *                                      raskas operaatio, joten se on syytä sisällyttää vain tarvittaessa.
+   * @return
+   */
   CompletableFuture<List<HakemusWrapper>> getApplicationsByHakukohde(
       String hakukohdeOid, boolean withHarkinnanvaraisuustieto);
-
-  CompletableFuture<List<HakemusWrapper>> getApplicationsByOids(List<String> oids);
-
-  CompletableFuture<List<HakemusWrapper>> getApplicationsByOidsWithHarkinnanvaraisuustieto(
-      List<String> oids);
-
-  Observable<List<AtaruSyntheticApplicationResponse>> putApplicationPrototypes(
-      Collection<AtaruHakemusPrototyyppi> hakemusPrototyypit);
 }
--- valintalaskenta-laskenta-service/src/main/java/fi/vm/sade/valinta/kooste/external/resource/oppijanumerorekisteri/OppijanumerorekisteriAsyncResource.java
+++ valintalaskenta-laskenta-service/src/main/java/fi/vm/sade/valinta/kooste/external/resource/oppijanumerorekisteri/OppijanumerorekisteriAsyncResource.java
@@ -1,19 +1,11 @@
 package fi.vm.sade.valinta.kooste.external.resource.oppijanumerorekisteri;
 
-import fi.vm.sade.valinta.kooste.external.resource.oppijanumerorekisteri.dto.HenkiloCreateDTO;
 import fi.vm.sade.valinta.kooste.external.resource.oppijanumerorekisteri.dto.HenkiloPerustietoDto;
-import fi.vm.sade.valinta.kooste.external.resource.oppijanumerorekisteri.dto.HenkiloViiteDto;
-import io.reactivex.Observable;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 
 public interface OppijanumerorekisteriAsyncResource {
-  Observable<List<HenkiloPerustietoDto>> haeTaiLuoHenkilot(
-      List<HenkiloCreateDTO> henkiloPrototyypit);
 
   CompletableFuture<Map<String, HenkiloPerustietoDto>> haeHenkilot(List<String> personOids);
-
-  CompletableFuture<List<HenkiloViiteDto>> haeHenkiloOidDuplikaatit(Set<String> personOids);
 }
