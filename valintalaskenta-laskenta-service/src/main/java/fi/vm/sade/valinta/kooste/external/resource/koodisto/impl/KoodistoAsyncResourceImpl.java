888ad04a68ca1d4cf68d4d2c5b1164768c97a4e8
Santeri Korri <santeri.korri@knowit.fi>
2024-09-24 10:14:02 +0300

    OK-611 Poistettu käyttämättömät metodit KoodistoAsyncResource-luokasta

--- valintalaskenta-laskenta-service/src/main/java/fi/vm/sade/valinta/kooste/external/resource/koodisto/impl/KoodistoAsyncResourceImpl.java
+++ valintalaskenta-laskenta-service/src/main/java/fi/vm/sade/valinta/kooste/external/resource/koodisto/impl/KoodistoAsyncResourceImpl.java
@@ -8,7 +8,6 @@ import fi.vm.sade.valinta.kooste.external.resource.koodisto.dto.Koodi;
 import java.time.Duration;
 import java.util.*;
 import java.util.concurrent.CompletableFuture;
-import java.util.stream.Collectors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,42 +24,6 @@ public class KoodistoAsyncResourceImpl implements KoodistoAsyncResource {
   public KoodistoAsyncResourceImpl(@Qualifier("KoodistoHttpClient") HttpClient client) {
     this.client = client;
     this.urlConfiguration = UrlConfiguration.getInstance();
-  }
-
-  @Override
-  public CompletableFuture<List<Koodi>> haeKoodisto(String koodistoUri) {
-    HashMap<String, Boolean> query = new HashMap<>();
-    query.put("onlyValidKoodis", true);
-    return this.client.getJson(
-        this.urlConfiguration.url("koodisto-service.json.oid.koodi", koodistoUri, query),
-        Duration.ofMinutes(1),
-        new TypeToken<List<Koodi>>() {}.getType());
-  }
-
-  @Override
-  public CompletableFuture<List<Koodi>> ylakoodit(String koodiUri) {
-    String[] parts = koodiUri.split("#");
-    Map<String, String> parameters = new HashMap<>();
-    if (parts.length > 1) {
-      parameters.put("koodiVersio", parts[1]);
-    }
-    return this.client.getJson(
-        this.urlConfiguration.url("koodisto-service.json.koodi.ylakoodit", parts[0], parameters),
-        Duration.ofMinutes(1),
-        new TypeToken<List<Koodi>>() {}.getType());
-  }
-
-  @Override
-  public CompletableFuture<List<Koodi>> alakoodit(String koodiUri) {
-    String[] parts = koodiUri.split("#");
-    Map<String, String> parameters = new HashMap<>();
-    if (parts.length > 1) {
-      parameters.put("koodiVersio", parts[1]);
-    }
-    return this.client.getJson(
-        this.urlConfiguration.url("koodisto-service.json.koodi.alakoodit", parts[0], parameters),
-        Duration.ofMinutes(1),
-        new TypeToken<List<Koodi>>() {}.getType());
   }
 
   @Override
@@ -95,16 +58,5 @@ public class KoodistoAsyncResourceImpl implements KoodistoAsyncResource {
         this.urlConfiguration.url("koodisto-service.json.koodi", koodiUri),
         Duration.ofMinutes(1),
         new TypeToken<Koodi>() {}.getType());
-  }
-
-  @Override
-  public CompletableFuture<List<Koodi>> haeKoodienUusinVersio(String... koodiUris) {
-    return this.client.getJson(
-        this.urlConfiguration.url("baseurl-koodisto-service")
-            + "/koodisto-service/rest/json/searchKoodis?koodiUris="
-            + Arrays.stream(koodiUris).collect(Collectors.joining(","))
-            + "&koodiversioSelection=LATEST",
-        Duration.ofMinutes(1),
-        new TypeToken<List<Koodi>>() {}.getType());
   }
 }
