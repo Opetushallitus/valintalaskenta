841e7f48316344f082b1212298e11635f704bb29
Santeri Korri <santeri.korri@knowit.fi>
2024-09-24 10:22:37 +0300

    OK-611 Poistettu käyttämätön koodi OrganisaatioAsyncResource-luokasta

--- valintalaskenta-laskenta-service/src/main/java/fi/vm/sade/valinta/kooste/external/resource/organisaatio/OrganisaatioAsyncResource.java
+++ valintalaskenta-laskenta-service/src/main/java/fi/vm/sade/valinta/kooste/external/resource/organisaatio/OrganisaatioAsyncResource.java
@@ -1,18 +1,8 @@
 package fi.vm.sade.valinta.kooste.external.resource.organisaatio;
 
-import fi.vm.sade.organisaatio.resource.dto.HakutoimistoDTO;
-import fi.vm.sade.valinta.kooste.external.resource.organisaatio.dto.Organisaatio;
-import fi.vm.sade.valinta.kooste.external.resource.organisaatio.dto.OrganisaatioTyyppiHierarkia;
-import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 
 public interface OrganisaatioAsyncResource {
-  CompletableFuture<Organisaatio> haeOrganisaatio(String organisaatioOid);
-
-  CompletableFuture<OrganisaatioTyyppiHierarkia> haeOrganisaationTyyppiHierarkia(
-      String organisaatioOid);
-
-  CompletableFuture<Optional<HakutoimistoDTO>> haeHakutoimisto(String organisaatioId);
 
   CompletableFuture<String> parentoids(String organisaatioId) throws Exception;
 }
