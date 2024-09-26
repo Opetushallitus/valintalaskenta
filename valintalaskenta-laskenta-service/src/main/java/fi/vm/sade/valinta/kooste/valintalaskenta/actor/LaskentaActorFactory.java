637279721b8a9514eee89e40eccbd9c27907471c
Santeri Korri <santeri.korri@knowit.fi>
2024-09-26 16:10:45 +0300

    OK-611 Poistettu toisteinen isValintaryhmaLaskenta-kentta LaskentaActorParams-luokasta

--- valintalaskenta-laskenta-service/src/main/java/fi/vm/sade/valinta/kooste/valintalaskenta/actor/LaskentaActorFactory.java
+++ valintalaskenta-laskenta-service/src/main/java/fi/vm/sade/valinta/kooste/valintalaskenta/actor/LaskentaActorFactory.java
@@ -113,7 +113,6 @@ public class LaskentaActorFactory {
         new LaskentaActorParams(
             a.getLaskentaStartParams(),
             Collections.singletonList(new HakukohdeJaOrganisaatio()));
-    fakeOnlyOneHakukohdeParams.setValintaryhmalaskenta(true);
     return laskentaHakukohteittainActor(
         laskentaSupervisor,
         fakeOnlyOneHakukohdeParams,
