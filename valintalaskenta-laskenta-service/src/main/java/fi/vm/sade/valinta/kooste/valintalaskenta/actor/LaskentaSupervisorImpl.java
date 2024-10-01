a4973b36fd39530cd38332e929a5f29e0a071ee3
Santeri Korri <santeri.korri@knowit.fi>
2024-10-01 10:40:18 +0300

    OK-611 Siirretty resurssien generointia lähemmäs käyttöä

--- valintalaskenta-laskenta-service/src/main/java/fi/vm/sade/valinta/kooste/valintalaskenta/actor/LaskentaSupervisorImpl.java
+++ valintalaskenta-laskenta-service/src/main/java/fi/vm/sade/valinta/kooste/valintalaskenta/actor/LaskentaSupervisorImpl.java
@@ -1,22 +1,15 @@
 package fi.vm.sade.valinta.kooste.valintalaskenta.actor;
 
 import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.Optional;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
-import fi.vm.sade.valinta.kooste.AuditSession;
-import fi.vm.sade.valinta.kooste.valintalaskenta.dto.HakukohdeJaOrganisaatio;
-import fi.vm.sade.valinta.kooste.valintalaskenta.dto.LaskentaStartParams;
-import fi.vm.sade.valinta.kooste.valintalaskenta.service.HakukohdeService;
 import fi.vm.sade.valintalaskenta.domain.dto.seuranta.HakukohdeTila;
 import fi.vm.sade.valintalaskenta.domain.dto.seuranta.IlmoitusDto;
 import fi.vm.sade.valintalaskenta.domain.dto.seuranta.LaskentaDto;
 import fi.vm.sade.valintalaskenta.domain.dto.seuranta.LaskentaTila;
-import fi.vm.sade.valintalaskenta.domain.dto.seuranta.LaskentaTyyppi;
 import fi.vm.sade.valintalaskenta.laskenta.dao.SeurantaDao;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -34,7 +27,6 @@ public class LaskentaSupervisorImpl {
   private static final Logger LOG = LoggerFactory.getLogger(LaskentaSupervisorImpl.class);
 
   private final LaskentaActorFactory laskentaActorFactory;
-  private final HakukohdeService hakukohdeService;
   private final SeurantaDao seurantaDao;
 
   private final int maxWorkers;
@@ -42,12 +34,10 @@ public class LaskentaSupervisorImpl {
 
   @Autowired
   public LaskentaSupervisorImpl(
-      HakukohdeService hakukohdeService,
       LaskentaActorFactory laskentaActorFactory,
       SeurantaDao seurantaDao,
       @Value("${valintalaskentakoostepalvelu.maxWorkerCount:8}") int maxWorkers) {
     this.laskentaActorFactory = laskentaActorFactory;
-    this.hakukohdeService = hakukohdeService;
     this.seurantaDao = seurantaDao;
     this.maxWorkers = maxWorkers;
     this.executorService =
@@ -75,53 +65,11 @@ public class LaskentaSupervisorImpl {
     }
   }
 
-  private static AuditSession koosteAuditSession(LaskentaDto laskenta) {
-    final String userAgent = "-";
-    final String inetAddress = "127.0.0.1";
-    AuditSession auditSession =
-        new AuditSession(laskenta.getUserOID(), Collections.emptyList(), userAgent, inetAddress);
-    auditSession.setSessionId(laskenta.getUuid());
-    auditSession.setPersonOid(laskenta.getUserOID());
-    return auditSession;
-  }
-
   private void startLaskenta(LaskentaDto laskenta) {
     LOG.info("Luodaan ja aloitetaan Laskenta uuid:lle {}", laskenta.getUuid());
 
-    LaskentaStartParams laskentaStartParams = new LaskentaStartParams(
-        koosteAuditSession(laskenta),
-        laskenta.getUuid(),
-        laskenta.getHakuOid(),
-        laskenta.isErillishaku(),
-        true,
-        LaskentaTyyppi.VALINTARYHMA.equals(laskenta.getTyyppi()),
-        laskenta.getValinnanvaihe(),
-        laskenta.getValintakoelaskenta(),
-        laskenta.getTyyppi());
-
-    Collection<HakukohdeJaOrganisaatio> hakukohteet;
     try {
-      hakukohteet = hakukohdeService.fetchHakukohteet(laskenta);
-    } catch (Throwable t) {
-      cancelLaskenta(
-          "Taustatietojen haku epäonnistui",
-          Optional.of(t),
-          laskenta.getUuid());
-      return;
-    }
-
-    if(hakukohteet.isEmpty()) {
-      cancelLaskenta(
-          "Haulla "
-              + laskenta.getUuid()
-              + " ei saatu hakukohteita! Onko valinnat synkronoitu tarjonnan kanssa?",
-          null,
-          laskenta.getUuid());
-      return;
-    }
-
-    try {
-      LaskentaActor laskentaActor = laskentaActorFactory.createLaskentaActor(laskentaStartParams, hakukohteet);
+      LaskentaActor laskentaActor = laskentaActorFactory.createLaskentaActor(laskenta);
       this.executorService.submit(() -> {
         try {
           laskentaActor.start();
@@ -129,8 +77,8 @@ public class LaskentaSupervisorImpl {
           cancelLaskenta("Laskenta päättyi virheeseen", Optional.of(t), laskenta.getUuid());
         }
       });
-    } catch (Exception e) {
-      this.cancelLaskenta("\r\n###\r\n### Laskenta uuid:lle {} haulle {} ei kaynnistynyt!\r\n###", Optional.of(e), laskentaStartParams.getUuid());
+    } catch (Throwable t) {
+      this.cancelLaskenta("\r\n###\r\n### Laskenta uuid:lle {} haulle {} ei kaynnistynyt!\r\n###", Optional.of(t), laskenta.getUuid());
     }
   }
 
