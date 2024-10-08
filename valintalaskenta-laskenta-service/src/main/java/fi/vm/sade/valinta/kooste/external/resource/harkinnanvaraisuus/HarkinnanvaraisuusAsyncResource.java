0015724b121f25f0493a2c08514ca84bd6eec4cd
Santeri Korri <santeri.korri@knowit.fi>
2024-10-08 15:12:51 +0300

    OK-611 Poistettu käyttämätöntä koodia

--- valintalaskenta-laskenta-service/src/main/java/fi/vm/sade/valinta/kooste/external/resource/harkinnanvaraisuus/HarkinnanvaraisuusAsyncResource.java
+++
@@ -1,9 +0,0 @@
-package fi.vm.sade.valinta.kooste.external.resource.harkinnanvaraisuus;
-
-import fi.vm.sade.valinta.kooste.external.resource.harkinnanvaraisuus.dto.HakemuksenHarkinnanvaraisuus;
-import fi.vm.sade.valinta.kooste.external.resource.suoritusrekisteri.dto.Oppija;
-
-public interface HarkinnanvaraisuusAsyncResource {
-
-  Boolean hasYksilollistettyMatAi(HakemuksenHarkinnanvaraisuus h, Oppija o);
-}
