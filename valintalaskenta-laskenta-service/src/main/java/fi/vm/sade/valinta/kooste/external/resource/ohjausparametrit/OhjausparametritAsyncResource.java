7a3284800f997e973c5049494b9ba996e89d8b7e
Santeri Korri <santeri.korri@knowit.fi>
2024-10-08 15:12:51 +0300

    OK-611 Poistettu käyttämätöntä koodia

--- valintalaskenta-laskenta-service/src/main/java/fi/vm/sade/valinta/kooste/external/resource/ohjausparametrit/OhjausparametritAsyncResource.java
+++
@@ -1,8 +0,0 @@
-package fi.vm.sade.valinta.kooste.external.resource.ohjausparametrit;
-
-import fi.vm.sade.valinta.kooste.external.resource.ohjausparametrit.dto.ParametritDTO;
-import java.util.concurrent.CompletableFuture;
-
-public interface OhjausparametritAsyncResource {
-  CompletableFuture<ParametritDTO> haeHaunOhjausparametrit(String hakuOid);
-}
