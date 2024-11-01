593e1fbc3b52ac3e930e13535a9052e771f6540f
Santeri Korri <santeri.korri@knowit.fi>
2024-11-01 16:27:20 +0200

    OK-611 Päivitetty spring boot -versio ja sharedutils-riippuvuus

--- valintalaskenta-tulos-service/src/main/java/fi/vm/sade/valintalaskenta/tulos/logging/LaskentaAuditLogMock.java
+++ valintalaskenta-tulos-service/src/main/java/fi/vm/sade/valintalaskenta/tulos/logging/LaskentaAuditLogMock.java
@@ -6,7 +6,7 @@ import fi.vm.sade.auditlog.Operation;
 import fi.vm.sade.auditlog.User;
 import fi.vm.sade.valinta.sharedutils.ValintaResource;
 import java.util.Map;
-import javax.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
