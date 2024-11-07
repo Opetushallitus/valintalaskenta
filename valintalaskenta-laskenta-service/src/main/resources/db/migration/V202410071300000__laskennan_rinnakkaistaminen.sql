6cae1203f37ccfdd3517a3ccad45ebac422e5aec
Santeri Korri <santeri.korri@knowit.fi>
2024-11-07 15:11:08 +0200

    OK-611 Lisätty timestampit hakukohteille

--- valintalaskenta-laskenta-service/src/main/resources/db/migration/V202410071300000__laskennan_rinnakkaistaminen.sql
+++ valintalaskenta-laskenta-service/src/main/resources/db/migration/V202410071300000__laskennan_rinnakkaistaminen.sql
@@ -1,5 +1,8 @@
-ALTER TABLE seuranta_laskenta_hakukohteet ADD COLUMN yritykset INTEGER DEFAULT 0;
-ALTER TABLE seuranta_laskenta_hakukohteet ADD COLUMN noodi_id TEXT;
+ALTER TABLE seuranta_laskenta_hakukohteet ADD COLUMN IF NOT EXISTS yritykset INTEGER DEFAULT 0;
+ALTER TABLE seuranta_laskenta_hakukohteet ADD COLUMN IF NOT EXISTS noodi_id TEXT;
+ALTER TABLE seuranta_laskenta_hakukohteet ADD COLUMN IF NOT EXISTS luotu TIMESTAMP;
+ALTER TABLE seuranta_laskenta_hakukohteet ADD COLUMN IF NOT EXISTS aloitettu TIMESTAMP;
+ALTER TABLE seuranta_laskenta_hakukohteet ADD COLUMN IF NOT EXISTS lopetettu TIMESTAMP;
 
 CREATE TABLE IF NOT EXISTS noodit (
                                       noodi_id  TEXT PRIMARY KEY,
@@ -11,4 +14,4 @@ CREATE TABLE IF NOT EXISTS parametrit (
                                       arvo  TEXT NOT NULL
 );
 
-INSERT INTO parametrit (nimi, arvo) VALUES ('maxYhtaaikaisetHakukohteet', '8');
+INSERT INTO parametrit (nimi, arvo) VALUES ('maxYhtaaikaisetHakukohteet', '8') ON CONFLICT DO NOTHING;
