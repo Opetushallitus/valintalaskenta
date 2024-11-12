303d9ad5d9ac7a1ffbf46260ce0e78cb2f0768b6
Santeri Korri <santeri.korri@knowit.fi>
2024-11-12 13:23:27 +0200

    OK-611 Muutettu valinnanvaihe optionaliksi

--- valintalaskenta-laskenta-service/src/main/java/fi/vm/sade/valinta/kooste/external/resource/valintaperusteet/ValintaperusteetAsyncResource.java
+++ valintalaskenta-laskenta-service/src/main/java/fi/vm/sade/valinta/kooste/external/resource/valintaperusteet/ValintaperusteetAsyncResource.java
@@ -2,6 +2,7 @@ package fi.vm.sade.valinta.kooste.external.resource.valintaperusteet;
 
 import fi.vm.sade.service.valintaperusteet.dto.*;
 import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 
 public interface ValintaperusteetAsyncResource {
@@ -11,5 +12,5 @@ public interface ValintaperusteetAsyncResource {
   CompletableFuture<String> haeValintaryhmaVastuuorganisaatio(String valintaryhmaOid);
 
   CompletableFuture<List<ValintaperusteetDTO>> haeValintaperusteet(
-      String hakukohdeOid, Integer valinnanVaiheJarjestysluku);
+      String hakukohdeOid, Optional<Integer> valinnanVaiheJarjestysluku);
 }
