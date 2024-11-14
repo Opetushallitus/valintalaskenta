2f935b700478a888f7f6545322709b9dc305bbf3
Santeri Korri <santeri.korri@knowit.fi>
2024-11-14 07:47:46 +0200

    OK-611 Uudelleennimetty paketit

---
+++ valintalaskenta-laskenta-service/src/main/java/fi/vm/sade/valintalaskenta/runner/resource/external/valintaperusteet/ValintaperusteetAsyncResource.java
@@ -0,0 +1,16 @@
+package fi.vm.sade.valintalaskenta.runner.resource.external.valintaperusteet;
+
+import fi.vm.sade.service.valintaperusteet.dto.*;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+
+public interface ValintaperusteetAsyncResource {
+
+  CompletableFuture<List<HakukohdeViiteDTO>> haunHakukohteet(String hakuOid);
+
+  CompletableFuture<String> haeValintaryhmaVastuuorganisaatio(String valintaryhmaOid);
+
+  CompletableFuture<List<ValintaperusteetDTO>> haeValintaperusteet(
+      String hakukohdeOid, Optional<Integer> valinnanVaiheJarjestysluku);
+}
