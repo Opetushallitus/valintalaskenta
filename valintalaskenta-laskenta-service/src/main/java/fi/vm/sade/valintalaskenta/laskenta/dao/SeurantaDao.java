54ab5e87cd530a14e8222d91316a92f65e833d41
Santeri Korri <santeri.korri@knowit.fi>
2024-11-14 07:53:56 +0200

    OK-611 Ajettu spotless

--- valintalaskenta-laskenta-service/src/main/java/fi/vm/sade/valintalaskenta/laskenta/dao/SeurantaDao.java
+++ valintalaskenta-laskenta-service/src/main/java/fi/vm/sade/valintalaskenta/laskenta/dao/SeurantaDao.java
@@ -1,35 +1,34 @@
 package fi.vm.sade.valintalaskenta.laskenta.dao;
 
 import fi.vm.sade.valintalaskenta.domain.dto.seuranta.*;
-import org.apache.commons.lang3.tuple.ImmutablePair;
-
 import java.time.Instant;
 import java.util.Collection;
 import java.util.Date;
 import java.util.Optional;
 import java.util.UUID;
+import org.apache.commons.lang3.tuple.ImmutablePair;
 
 /**
  * Laskentojen koordinointiin liittyvät tietokantatoiminnot.
  *
- * Useat metodit sisältävät useampia tietokantakutsuja. Jos transaktio on jo käynnissä, ne suoritetaan osana olemassaolevaa
- * transaktiota, muuten käynnistetään uusi transaktio. Huomioi tämä suunnittelulähtökohta luokkaa muuttaessasi.
+ * <p>Useat metodit sisältävät useampia tietokantakutsuja. Jos transaktio on jo käynnissä, ne
+ * suoritetaan osana olemassaolevaa transaktiota, muuten käynnistetään uusi transaktio. Huomioi tämä
+ * suunnittelulähtökohta luokkaa muuttaessasi.
  */
 public interface SeurantaDao {
 
   /**
    * Luo uuden laskennan
    *
-   * @param userOID             laskennan käynnistäneen käyttäjän tunniste
-   * @param haunnimi            haun nimi (raportointinäkymää varten)
-   * @param nimi                laskennan nimi (raportointinäkymää varten)
-   * @param hakuOid             haun tunniste
-   * @param tyyppi              haun tyyppi
-   * @param erillishaku         onko kyseessä erillishaku
-   * @param valinnanvaihe       laskettava valinnanvaihe (empty tarkoittaa että lasketaan kaikki vaiheet)
-   * @param valintakoelaskenta  onko kyseessä valintakoelaskenta
-   * @param hakukohdeOids       laskettavat hakukohteet
-   *
+   * @param userOID laskennan käynnistäneen käyttäjän tunniste
+   * @param haunnimi haun nimi (raportointinäkymää varten)
+   * @param nimi laskennan nimi (raportointinäkymää varten)
+   * @param hakuOid haun tunniste
+   * @param tyyppi haun tyyppi
+   * @param erillishaku onko kyseessä erillishaku
+   * @param valinnanvaihe laskettava valinnanvaihe (empty tarkoittaa että lasketaan kaikki vaiheet)
+   * @param valintakoelaskenta onko kyseessä valintakoelaskenta
+   * @param hakukohdeOids laskettavat hakukohteet
    * @return
    */
   TunnisteDto luoLaskenta(
@@ -51,54 +50,58 @@ public interface SeurantaDao {
   Optional<LaskentaDto> haeLaskenta(String uuid);
 
   /**
-   * Ottaa seuraavan hakukohteen työn alle. Jos kyseessä on valintaryhmälaskenta, kaikki hakukohteet otetaan työn
-   * alle samalla kertaa.
+   * Ottaa seuraavan hakukohteen työn alle. Jos kyseessä on valintaryhmälaskenta, kaikki hakukohteet
+   * otetaan työn alle samalla kertaa.
    *
-   * @param noodiId                     noodi joka on käynnistämässä hakukohteen laskentaa
-   * @param maxYhtaaikaisetHakukohteet  maksimimäärä hakukohteita joka voi olla käynnissä yhdellä noodilla yhtäaikaa
-   *
-   * @return                            käynnistetty hakukohde (hakukohteet)
+   * @param noodiId noodi joka on käynnistämässä hakukohteen laskentaa
+   * @param maxYhtaaikaisetHakukohteet maksimimäärä hakukohteita joka voi olla käynnissä yhdellä
+   *     noodilla yhtäaikaa
+   * @return käynnistetty hakukohde (hakukohteet)
    */
-  Optional<ImmutablePair<UUID, Collection<String>>> otaSeuraavatHakukohteetTyonAlle(String noodiId, int maxYhtaaikaisetHakukohteet);
+  Optional<ImmutablePair<UUID, Collection<String>>> otaSeuraavatHakukohteetTyonAlle(
+      String noodiId, int maxYhtaaikaisetHakukohteet);
 
   /**
-   * Peruuttaa luodun laskennan, ts. merkitsee laskennan tilan peruutetuksi ja hakukohteiden laskennan tilan
-   * keskeytetyksi
+   * Peruuttaa luodun laskennan, ts. merkitsee laskennan tilan peruutetuksi ja hakukohteiden
+   * laskennan tilan keskeytetyksi
    *
-   * @param uuid      keskeytettävän laskennan tunniste
-   * @param ilmoitus  laskennan keskeyttämisen syy
+   * @param uuid keskeytettävän laskennan tunniste
+   * @param ilmoitus laskennan keskeyttämisen syy
    */
   void peruutaLaskenta(String uuid, Optional<IlmoitusDto> ilmoitus);
 
   /**
-   * Resetoi laskennan, ts. siirtää laskennan tilaan ALOITTAMATTA, ja keskeytyneet hakukohteet tilaan TEKEMATTA
+   * Resetoi laskennan, ts. siirtää laskennan tilaan ALOITTAMATTA, ja keskeytyneet hakukohteet
+   * tilaan TEKEMATTA
    *
-   * @param uuid              laskennan tunniste
+   * @param uuid laskennan tunniste
    * @param nollaaIlmoitukset poistetaanko laskentaa liitetyt ilmoitukset
-   *
-   * @return  resetoitu laskenta
+   * @return resetoitu laskenta
    */
   LaskentaDto resetoiLaskenta(String uuid, boolean nollaaIlmoitukset);
 
   /**
-   * Merkitsee lasketut hakukohteet valmiiksi. Mikäli laskennan kaikki hakukohteet on laskettu onnistuneesti merkitään
-   * laskenta valmiiksi. Mikäli osan hakukohteista on epäonnistunut siirretään laskenta virhetilaan.
+   * Merkitsee lasketut hakukohteet valmiiksi. Mikäli laskennan kaikki hakukohteet on laskettu
+   * onnistuneesti merkitään laskenta valmiiksi. Mikäli osan hakukohteista on epäonnistunut
+   * siirretään laskenta virhetilaan.
    *
-   * @param uuid          laskennan tunniste
+   * @param uuid laskennan tunniste
    * @param hakukohdeOids valmiiksi merkittävät hakukohteet
    */
   void merkkaaHakukohteetValmiiksi(UUID uuid, Collection<String> hakukohdeOids);
 
   /**
-   * Merkitsee epäonnistuneet hakukohteet epäonnistuneiksi jos maksimimäärä yrityksiä on käytetty, muuten siirtää ne
-   * TEKEMÄTTÄ-tilaan.
+   * Merkitsee epäonnistuneet hakukohteet epäonnistuneiksi jos maksimimäärä yrityksiä on käytetty,
+   * muuten siirtää ne TEKEMÄTTÄ-tilaan.
    *
-   * @param uuid          laskennan tunniste
+   * @param uuid laskennan tunniste
    * @param hakukohdeOids epäonnistuneet hakukohteet
-   * @param maxYritykset  maksimäärä yrityksiä jonka jälkeen hakukohteen laskenta todetaan epäonnistuneeksi
-   * @param message       epäonnistumisen syy
+   * @param maxYritykset maksimäärä yrityksiä jonka jälkeen hakukohteen laskenta todetaan
+   *     epäonnistuneeksi
+   * @param message epäonnistumisen syy
    */
-  void merkkaaHakukohteetEpaonnistuneeksi(UUID uuid, Collection<String> hakukohdeOids, int maxYritykset, String message);
+  void merkkaaHakukohteetEpaonnistuneeksi(
+      UUID uuid, Collection<String> hakukohdeOids, int maxYritykset, String message);
 
   /**
    * Merkkaa laskentaa suorittavan noodin elossaolevaksi
@@ -108,8 +111,8 @@ public interface SeurantaDao {
   void merkkaaNoodiLiveksi(String noodiId);
 
   /**
-   * Resetoi niillä noodeilla käynnissä olevat hakukohteiden laskennat jotka eivät ole ilmoittaneet olevansa elossa
-   * tietyn ajan sisällä.
+   * Resetoi niillä noodeilla käynnissä olevat hakukohteiden laskennat jotka eivät ole ilmoittaneet
+   * olevansa elossa tietyn ajan sisällä.
    *
    * @param viive kuinka uusi viimeisimmän elonmerkin pitää olla
    */
@@ -139,8 +142,8 @@ public interface SeurantaDao {
   /**
    * Lukee parametrin tietokannasta
    *
-   * @param nimi  parametrin nimi
-   * @return      parametrin arvo
+   * @param nimi parametrin nimi
+   * @return parametrin arvo
    */
   String lueParametri(String nimi);
 }
