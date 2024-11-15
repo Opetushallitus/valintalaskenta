f81fc4d5fce2b00ff3bf00874c46f2e7b2873680
Santeri Korri <santeri.korri@knowit.fi>
2024-11-15 15:35:41 +0200

    OK-611 Tallennetaan laskennan kestot CloudWatchiin

--- valintalaskenta-laskenta-service/src/test/java/fi/vm/sade/valintalaskenta/laskenta/DevApp.java
+++ valintalaskenta-laskenta-service/src/test/java/fi/vm/sade/valintalaskenta/laskenta/DevApp.java
@@ -1,15 +1,49 @@
 package fi.vm.sade.valintalaskenta.laskenta;
 
+import static org.testcontainers.containers.localstack.LocalStackContainer.Service.CLOUDWATCH;
+
+import com.github.dockerjava.api.model.ExposedPort;
+import com.github.dockerjava.api.model.HostConfig;
+import com.github.dockerjava.api.model.PortBinding;
+import com.github.dockerjava.api.model.Ports;
 import fi.vm.sade.valintalaskenta.App;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.annotation.Profile;
+import org.springframework.test.util.TestSocketUtils;
+import org.testcontainers.containers.localstack.LocalStackContainer;
+import org.testcontainers.utility.DockerImageName;
 
 @Profile("dev")
 public class DevApp {
 
+  private static final Logger LOG = LoggerFactory.getLogger(DevApp.class);
+
   private static final String ENVIRONMENT = "untuva";
 
+  private static final int localstackPort = TestSocketUtils.findAvailableTcpPort();
+
+  private static final LocalStackContainer localStackContainer =
+      new LocalStackContainer(new DockerImageName("localstack/localstack:2.2.0"))
+          .withServices(CLOUDWATCH)
+          .withLogConsumer(frame -> LOG.info(frame.getUtf8StringWithoutLineEnding()))
+          .withExposedPorts(4566)
+          .withCreateContainerCmdModifier(
+              m ->
+                  m.withHostConfig(
+                      new HostConfig()
+                          .withPortBindings(
+                              new PortBinding(
+                                  Ports.Binding.bindPort(localstackPort), new ExposedPort(4566)))));
+
   public static void main(String[] args) {
+    localStackContainer.start();
+
+    System.setProperty("localstackPort", localstackPort + "");
+    System.setProperty("aws.accessKeyId", "localstack");
+    System.setProperty("aws.secretAccessKey", "localstack");
+
     System.setProperty("spring.profiles.active", "dev");
 
     System.setProperty(
