ad66981023e19f100a16530fedad13b74e5412d7
Matti Pitkämäki <matti.pitkamaki@gofore.com>
2025-01-31 14:30:54 +0200

    OK-450: Replaced failing javax -dependency

--- valintalaskenta-laskenta-service/src/main/java/fi/vm/sade/valintalaskenta/laskenta/dao/impl/HakijaryhmaDAOImpl.java
+++ valintalaskenta-laskenta-service/src/main/java/fi/vm/sade/valintalaskenta/laskenta/dao/impl/HakijaryhmaDAOImpl.java
@@ -15,7 +15,7 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
-import javax.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.stereotype.Service;
 
 @Service
