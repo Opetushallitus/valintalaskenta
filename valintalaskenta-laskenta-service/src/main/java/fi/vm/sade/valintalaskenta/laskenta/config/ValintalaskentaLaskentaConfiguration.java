73d57fd759aa133b5a1f4fee50b9b60f68d7a92d
Santeri Korri <santeri.korri@knowit.fi>
2025-02-04 09:20:10 +0200

    OY-4988 Poistettu CORS-headerit

--- valintalaskenta-laskenta-service/src/main/java/fi/vm/sade/valintalaskenta/laskenta/config/ValintalaskentaLaskentaConfiguration.java
+++ valintalaskenta-laskenta-service/src/main/java/fi/vm/sade/valintalaskenta/laskenta/config/ValintalaskentaLaskentaConfiguration.java
@@ -30,8 +30,6 @@ import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilde
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @ComponentScan(
@@ -180,16 +178,6 @@ public class ValintalaskentaLaskentaConfiguration {
               clientConnectionTimeout,
               clientReceiveTimeout);
       return result;
-    };
-  }
-
-  @Bean
-  public WebMvcConfigurer corsConfigurer() {
-    return new WebMvcConfigurer() {
-      @Override
-      public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/").allowedOrigins("*");
-      }
     };
   }
 
