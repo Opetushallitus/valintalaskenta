a6ddab8787f17a24c8cf6dd5f76c1c054911b6ce
Risto Salama <risto.salama@gofore.com>
2025-03-05 10:16:12 +0200

    Rajapinta joka palauttaa haun hakukohdeoidit tiedolla milloin ne on laskettu

--- valintalaskenta-laskenta-service/src/test/java/fi/vm/sade/valintalaskenta/tulos/dao/TulosValinnanvaiheDaoTest.java
+++ valintalaskenta-laskenta-service/src/test/java/fi/vm/sade/valintalaskenta/tulos/dao/TulosValinnanvaiheDaoTest.java
@@ -2,10 +2,13 @@ package fi.vm.sade.valintalaskenta.tulos.dao;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import fi.vm.sade.valintalaskenta.domain.testdata.TestEntityDataUtil;
+import fi.vm.sade.valintalaskenta.domain.valinta.HakukohdeLaskentaTehty;
 import fi.vm.sade.valintalaskenta.domain.valinta.Valinnanvaihe;
 import fi.vm.sade.valintalaskenta.testing.AbstractIntegrationTest;
+import java.util.Date;
 import java.util.HashSet;
 import java.util.List;
 import org.junit.jupiter.api.Test;
@@ -31,5 +34,34 @@ public class TulosValinnanvaiheDaoTest extends AbstractIntegrationTest {
 
     assertNotNull(vaihe);
     assertEquals("vaihe1", vaihe.getValinnanVaiheOid());
+  }
+
+  @Test
+  public void testHakeeViimeisimmätHaunHakukohteetLaskentaTehtyTiedoilla()
+      throws InterruptedException {
+    Valinnanvaihe vv1 = luoValinnanVaiheAIkaLeimalla("vaihe1", 1);
+    Valinnanvaihe vv2 = luoValinnanVaiheAIkaLeimalla("vaihe2", 2);
+    Valinnanvaihe vv3 = luoValinnanVaiheAIkaLeimalla("vaihe3", 3);
+    valinnanvaiheRepository.saveAll(List.of(vv1, vv2, vv3));
+    // varmistetaan ajan kulu jotta testi on deterministinen
+    Thread.sleep(1L);
+    Date timeNow = new Date();
+    valinnanvaiheRepository.save(luoValinnanVaiheAIkaLeimalla("vaihe4", 4));
+    List<HakukohdeLaskentaTehty> tehdytLaskennat =
+        tulosValinnanvaiheDAO.haeLasketutHakukohteetHaulle("haku1");
+    assertEquals(1, tehdytLaskennat.size());
+    Date modified = tehdytLaskennat.get(0).lastModified;
+    assertTrue(timeNow.before(modified) || timeNow.equals(modified));
+  }
+
+  private Valinnanvaihe luoValinnanVaiheAIkaLeimalla(String vaiheOid, int jarjestysnro) {
+    return TestEntityDataUtil.luoValinnanvaiheEntity(
+        "haku1",
+        "hakukohde1",
+        jarjestysnro,
+        vaiheOid,
+        List.of(
+            TestEntityDataUtil.luoValintatapaJonoEntity(
+                0, new HashSet<>(), "jono1nimi" + jarjestysnro, 0, null, "jono1" + jarjestysnro)));
   }
 }
