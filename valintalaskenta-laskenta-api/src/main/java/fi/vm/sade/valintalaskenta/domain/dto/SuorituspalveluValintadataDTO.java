86982e60498c63c508d90d3edda9b64e7bae0e02
Mikko Siukola <mikko.siukola@cybercom.com>
2025-11-14 14:08:19 +0200

    OPHSUPA-126 Compare Koostepalvelu and Supa key-values

--- valintalaskenta-laskenta-api/src/main/java/fi/vm/sade/valintalaskenta/domain/dto/SuorituspalveluValintadataDTO.java
+++ valintalaskenta-laskenta-api/src/main/java/fi/vm/sade/valintalaskenta/domain/dto/SuorituspalveluValintadataDTO.java
@@ -1,7 +1,6 @@
 package fi.vm.sade.valintalaskenta.domain.dto;
 
 import java.util.List;
-import java.util.Optional;
 
 public class SuorituspalveluValintadataDTO {
 
@@ -9,19 +8,6 @@ public class SuorituspalveluValintadataDTO {
 
   public SuorituspalveluValintadataDTO(List<HakemusDTO> valintaHakemukset) {
     this.valintaHakemukset = valintaHakemukset;
-  }
-
-  public void vertaile(List<HakemusDTO> koostepalveluSureHakemukset) {
-    koostepalveluSureHakemukset.forEach(
-        sureHakemus -> {
-          Optional<HakemusDTO> supaHakemus =
-              valintaHakemukset.stream()
-                  .filter(vh -> vh.getHakemusoid().equals(sureHakemus.getHakemusoid()))
-                  .findFirst();
-          if (supaHakemus.isPresent()) {
-            // ...
-          }
-        });
   }
 
   public List<HakemusDTO> getValintaHakemukset() {
