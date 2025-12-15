b903ec3fa921e60dd0c043ef77fb6ae9aeb6bd6e
Topias Heinonen <topias.heinonen@gofore.com>
2025-12-15 11:41:41 +0200

    OY-5423 Poistetaan vanha migraatioskripti (#151)
    Mongo -> Postgresql migraatio on tehty, niin tätä skriptiä ei enää tarvita. Tarvittaessa se löytyy versiohistoriasta, jos sitä halutaan joskus tarkastella.

--- mongo-to-postgres/index.js
+++
@@ -1,8 +0,0 @@
-import { argv } from 'node:process';
-import migrate from "./src/migrate.js";
-import {connections, collections, collectionsForHaku} from './settings.js';
-
-const useAfter = !!argv.find(a => a == 'useAfter');
-
-migrate({connections, collections, collectionsForHaku, useAfter});
-
